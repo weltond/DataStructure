24d5d676a09c24442938e29db75ce08f99ac7141
hongyuan_weltond <ningh@umich.edu>
2019-02-19 23:39:50 -0500

    Update Lc90SubsetsII.java

--- LeetCode/backtracking/Lc90SubsetsII.java
+++ LeetCode/backtracking/Lc90SubsetsII.java
@@ -34,3 +34,31 @@ class Solution {
         
     }
 }
+
+/*Another Solution*/
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if(nums == null || nums.length == 0){
+            return res;
+        }
+        Arrays.sort(nums);
+        List<Integer> curr = new ArrayList<>();
+        dfs(nums, 0, res, curr);
+        return res;
+    }
+    private void dfs(int[] nums, int index, List<List<Integer>> res, List<Integer> curr){
+        res.add(new ArrayList<>(curr));
+        if(index == nums.length){
+            return;
+        }
+        Set<Integer> visited = new HashSet<Integer>();
+        for(int i = index; i < nums.length; i++){
+            if(visited.add(nums[i])){
+                curr.add(nums[i]);
+                dfs(nums, i + 1, res, curr);
+                curr.remove(curr.size() - 1);
+            } 
+        }
+    }
+}
