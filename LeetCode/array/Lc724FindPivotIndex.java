e5c0e515b1e117f452a5f60b5e6dd9056c25f75d
hongyuan_weltond <ningh@umich.edu>
2019-02-27 10:54:32 -0500

    Create Lc724FindPivotIndex.java

---
+++ LeetCode/array/Lc724FindPivotIndex.java
@@ -0,0 +1,42 @@
+//https://leetcode.com/problems/find-pivot-index/
+
+class Solution {
+    // ==== Sol 1: First attempt =====
+    public int pivotIndex(int[] nums) {
+        if (nums == null || nums.length == 0) return -1;
+        
+        int[] prefixSum = build(nums);
+
+        for (int i = 1; i < prefixSum.length; i++) {
+            if (prefixSum[prefixSum.length - 1] - prefixSum[i] == prefixSum[i - 1]) {
+                return i - 1;
+            }
+        }
+        return -1;
+    }
+    
+    private int[] build(int[] nums) {
+        int[] arr = new int[nums.length + 1];   // first num is always 0.
+        
+        for (int i = 1; i <= nums.length; i++) {
+            arr[i] = nums[i - 1] + arr[i - 1];    
+        }
+        
+        return arr;
+    }
+    
+    // ==== Sol 2: More Elegant =====
+    public int pivotIndex(int[] nums) {
+        int sum = 0;
+		for(int i = 0; i<nums.length; i++) sum +=nums[i];
+		int pivotSum = 0;
+		for(int i = 0; i<nums.length; i++) {
+			if(sum - pivotSum - nums[i] == pivotSum) {
+				return i;
+			}
+			pivotSum = pivotSum + nums[i];
+		}
+		 
+		return -1;
+    }
+}
