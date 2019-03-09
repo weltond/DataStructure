6ba0149a230b65acc8fc09bda3f10506d7dd02a9
hongyuan_weltond <ningh@umich.edu>
2019-03-09 11:31:13 -0500

    Update Lc154FindMinInRotatedSortedArrayII.java

--- LeetCode/search/binarysearch/Lc154FindMinInRotatedSortedArrayII.java
+++ LeetCode/search/binarysearch/Lc154FindMinInRotatedSortedArrayII.java
@@ -1,4 +1,23 @@
 // https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/
+
+// ============ Template II ===============
+class Solution {
+    public int findMin(int[] nums) {
+        if (nums == null) return -1;
+        
+        int left = 0, right = nums.length - 1;
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[right]) left = mid + 1;
+            else if (nums[mid] < nums[right]) right = mid;
+            else right--;
+        }
+        return nums[left];
+    }
+}
+
+// ============ Template III ===============
 class Solution {
     public int findMin(int[] nums) {
         if (nums == null || nums.length == 0) return -1;
