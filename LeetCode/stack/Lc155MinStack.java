8580d32da6eb2b29a9884bb2d5a42bb08c5e38d7
hongyuan_weltond <ningh@umich.edu>
2019-03-11 13:40:01 -0400

    Create Lc155MinStack.java

---
+++ LeetCode/stack/Lc155MinStack.java
@@ -0,0 +1,123 @@
+//https://leetcode.com/problems/min-stack/
+
+class MinStack {
+    List<Integer> arr;
+    int globalMin;
+    /** initialize your data structure here. */
+    public MinStack() {
+        arr = new ArrayList();
+        globalMin = Integer.MAX_VALUE;
+    }
+    
+    // ======= Elegant Solution =====
+    /** Main Idea:
+    Push oldMin into stack with newMin together when need to update min
+    Pop twice if the top is min value and set the globalMin to the second pop value
+    */
+    
+    public void push(int x) {
+        if (globalMin >= x) {   // IMPORTANT: has to be also equal. Otherwise it will influence pop()
+            arr.add(globalMin); // push old_globalMin into stack first
+            globalMin = x;
+        }
+        
+        arr.add(x);
+    }
+    
+    public void pop() {
+        if (isEmpty()) return;
+        
+        int tmp = top();
+        arr.remove(arr.size() - 1);
+        // if top() is globalMin, then it should remove the next again because 
+        // If needed, every time we push old globalMin before we push new globalMin into stack
+        if (tmp == globalMin) {
+            globalMin = top();
+            arr.remove(arr.size() - 1);
+        }
+    }
+    
+    public int top() {
+        return isEmpty() ? Integer.MAX_VALUE : arr.get(arr.size() - 1);
+    }
+    
+    public int getMin() {
+        return globalMin;
+    }
+    
+    private boolean isEmpty() {
+        return arr.size() == 0;
+    }
+    
+    // ======= Not Solution ======
+    // Following will cause overflow if x is greater than Max_VALUE / 2.
+    /** Main Idea
+    1. Push:
+        a) if greater than min, just push
+        b) if smaller than min, push 2 * x - min. update min
+    2. Pop:
+        a) if greater than min, just pop
+        b) if smaller than min, pop 2 * min - top(). update min
+    */
+
+    
+//     public void push(int x) {
+//         // if empty, first value is global min.
+//         if (isEmpty()) {
+//             globalMin = x;
+//             arr.add(x);
+//             return;
+//         }
+        
+//         // if not empty:
+//         // a) x >= globalMin
+//         if ((long)x >= globalMin) {
+//             arr.add(x);
+//         } 
+//         // b) x < globalMin, push 2 * x - old_globalMin into stack
+//         else {
+//             arr.add(2 * x - globalMin);
+//             globalMin = x;
+//         }
+//     }
+    
+//     public void pop() {
+//         if (arr.size() <= 0) return;
+        
+//         int myTop = top();
+//         // a) if top greater than globalMin, just pop
+        
+//         // b) top smaller than globalMin, update globalMin to 2 * globalMin - myTop
+//         //    -> myTop: the one push to stack after calc: 2 * x - old_globalMin
+//         //    -> globalMin: x
+//         if (myTop < globalMin) {
+//             globalMin = 2 * globalMin - myTop;
+//         }
+        
+//         arr.remove(arr.size() - 1);
+        
+//     }
+    
+//     public int top() {
+//         int tmp = arr.get(arr.size() - 1);
+//         if (tmp < globalMin) return globalMin;
+//         else return tmp;
+//     }
+    
+//     public int getMin() {
+//         return globalMin;
+//     }
+    
+//     private boolean isEmpty() {
+//         return arr.size() == 0;
+//     }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
