b1dc00dfea3b71f03a89a4c843fcff2ea1790e55
hongyuan_weltond <ningh@umich.edu>
2019-03-14 22:08:32 -0400

    Update Lc729MyCalendarI.java

--- LeetCode/tree/Lc729MyCalendarI.java
+++ LeetCode/tree/Lc729MyCalendarI.java
@@ -14,6 +14,8 @@ SO-> We need a data structure that keeps elements sorted and supports fast inser
 TreeMap
 
 */
+// ============= Method 1: BST ===========
+// 78ms
 class MyCalendar {
     TreeMap<Integer, Integer> calendar; // <start, end>
     public MyCalendar() {
@@ -33,6 +35,49 @@ class MyCalendar {
     }
 }
 
+// ============== Method 2: BST Like Calendar II ==========
+// 242ms
+class MyCalendar {
+    TreeMap<Integer, Integer> delta;
+    public MyCalendar() {
+        delta = new TreeMap();
+    }
+    
+    public boolean book(int start, int end) {
+        delta.put(start, delta.getOrDefault(start, 0) + 1);
+        delta.put(end, delta.getOrDefault(end, 0) - 1);
+        
+        int active = 0;
+        for (Map.Entry<Integer, Integer> entry : delta.entrySet()) {
+            int key = entry.getKey();
+            int val = entry.getValue();
+            
+            active += val;
+            
+            // there is a conflict
+            if (active == 2) {
+                // reset the start and end
+                delta.put(start, delta.get(start) - 1);
+                delta.put(end, delta.get(end) + 1);
+                if (delta.get(start) == 0){
+                    delta.remove(start);
+                }
+                if (delta.get(end) == 0) {
+                    delta.remove(end);
+                }  
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar obj = new MyCalendar();
+ * boolean param_1 = obj.book(start,end);
+ */
 /**
  * Your MyCalendar object will be instantiated and called as such:
  * MyCalendar obj = new MyCalendar();
