92cebff3b6238b21ffb5dca4468c21f1a5cab064
hongyuan_weltond <ningh@umich.edu>
2019-03-21 10:20:02 -0400

    Create Lc160IntersectionOfTwoLinkedList.java

---
+++ LeetCode/linkedlist/Lc160IntersectionOfTwoLinkedList.java
@@ -0,0 +1,64 @@
+// https://leetcode.com/problems/intersection-of-two-linked-lists/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    
+    // ===== Method 2: Two Pointers ======
+    // 1ms
+    // Time = O(m+n) Space = O(1)
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+        ListNode node = null;
+        ListNode tmpA = headA, tmpB = headB;
+        while (tmpA != tmpB) {
+            /** NOT ELEGANT!!!!
+            if (tmpA == null && tmpB == null) return null;
+            if (tmpA == null) tmpA = headB;
+            if (tmpB == null) tmpB = headA;
+            if (tmpA == tmpB && tmpA != null) return tmpA;
+            
+            tmpA = tmpA.next;
+            tmpB = tmpB.next;
+            */
+            
+            tmpA = tmpA == null ? headB : tmpA.next;
+            tmpB = tmpB == null ? headA : tmpB.next;
+        }
+        
+        return tmpA;
+    }
+    
+    
+    // ===== Method 1: Hash Table ======
+    // 8ms
+    // Time = O(m + n) Space = O(m) or O(n)
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+        
+        Set<ListNode> set = new HashSet();
+        
+        while (headA != null) {
+            set.add(headA);
+            headA = headA.next;
+        }
+        
+        while (headB != null) {
+            if (set.contains(headB)) {
+                return headB;
+            }
+            headB = headB.next;
+        }
+        
+        return null;
+    }
+}
