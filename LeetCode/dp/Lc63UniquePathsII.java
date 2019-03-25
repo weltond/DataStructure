7d395c505e4522aae8b483ef0b98fdee235f7df7
hongyuan_weltond <ningh@umich.edu>
2019-03-24 21:49:28 -0400

    Create Lc63UniquePathsII.java

---
+++ LeetCode/dp/Lc63UniquePathsII.java
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/unique-paths-ii/
+
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if (obstacleGrid == null || obstacleGrid.length == 0) return 0;
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        
+        int[][] dp = new int[m + 1][n + 1];
+        dp[0][1] = 1;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (obstacleGrid[i - 1][j - 1] == 1) {
+                    dp[i][j] = 0;
+                    continue;
+                }
+                
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        
+        return dp[m][n];
+    }
+}
