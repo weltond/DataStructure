6f9146a2821996cd3534065be59d886c711dd88f
hongyuan_weltond <ningh@umich.edu>
2019-03-25 09:53:46 -0400

    Create Lc210CourseScheduleII.java

---
+++ LeetCode/graph/Lc210CourseScheduleII.java
@@ -0,0 +1,53 @@
+// https://leetcode.com/problems/course-schedule-ii/
+
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        if (prerequisites == null) return new int[]{0};
+        
+        List<List<Integer>> graph = buildGraph(numCourses, prerequisites);
+        
+        // get indegree for each course.
+        int[] indegree = new int[numCourses];
+        for (int i = 0; i < prerequisites.length; i++) {
+            indegree[prerequisites[i][0]]++; 
+        }
+        
+        Queue<Integer> q = new LinkedList();
+        // if course indegree is 0, which means it doesn't rely on any other courses
+        // put them into the queue
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0)
+                q.offer(i);
+        }
+        
+        int cnt = 0;    // used to verify no circle
+        int[] res = new int[numCourses];
+        while (!q.isEmpty()) {
+            int course = q.poll();
+            res[cnt] = course;
+
+            for (Integer next : graph.get(course)) {
+                if (--indegree[next] == 0)
+                    q.offer(next);
+            }
+            
+            cnt++;
+        }
+        
+        return cnt == numCourses ? res : new int[]{};
+    }
+    
+    private List<List<Integer>> buildGraph(int num, int[][] arr) {
+        List<List<Integer>> graph = new ArrayList();
+        
+        for (int i = 0; i < num; i++) {
+            graph.add(new ArrayList());
+        }
+        
+        for (int i = 0; i < arr.length; i++) {
+            graph.get(arr[i][1]).add(arr[i][0]);
+        }
+        
+        return graph;
+    }
+}
