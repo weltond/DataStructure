f388a2e2b005d03fc021529f37193d65c9ee4e7e
hongyuan_weltond <ningh@umich.edu>
2019-03-25 15:17:41 -0400

    Update Lc373FindKPiarsWithSmallestSums.java

--- LeetCode/heap/Lc373FindKPiarsWithSmallestSums.java
+++ LeetCode/heap/Lc373FindKPiarsWithSmallestSums.java
@@ -23,6 +23,8 @@ Input: nums1 = [1,2], nums2 = [3], k = 3
 Output: [1,3],[2,3]
 Explanation: All possible pairs are returned from the sequence: [1,3],[2,3]
 */
+
+// Approach 1:
 class Solution {
     public List<int[]> kSmallestPairs(int[] nums1, int[] nums2, int k) {
         // min heap
@@ -51,5 +53,52 @@ class Solution {
         }
         
         return res;
+    }
+}
+
+// Approach 2: 33ms(55.28%)
+class Solution {
+    // 1. put k pair of (nums1[0], nums2[0..k-1]) into pq.
+    // 2. poll from pq. offer(nums1[i+1],nums1[j]) into pq
+    public List<int[]> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+        if (nums1 == null || nums2 == null) return new ArrayList();
+        
+        if (nums1.length == 0 || nums2.length == 0 || k == 0) return new ArrayList();
+        
+        // min heap
+        PriorityQueue<Pair> pq = new PriorityQueue<Pair>(k + 1, (o1, o2) -> {
+            return (nums1[o1.v1] + nums2[o1.v2]) - (nums1[o2.v1] + nums2[o2.v2]); 
+        });
+        
+        // offer k pairs into pq.
+        for (int j = 0; j < nums2.length && j <= k; j++) {
+            pq.offer(new Pair(0, j));
+        }
+        
+        
+        List<int[]> res = new ArrayList();
+        
+        while (k-- > 0 && !pq.isEmpty()) {
+            Pair cur = pq.poll();
+            int idx1 = cur.v1;
+            int idx2 = cur.v2;
+            res.add(new int[]{nums1[idx1], nums2[idx2]});
+            if (idx1 + 1 == nums1.length) continue;
+            
+            pq.offer(new Pair(idx1 + 1, idx2));
+        }
+        
+        return res;
+        
+    }
+    
+    class Pair{
+        int v1;
+        int v2;
+        // index pair
+        Pair(int v1, int v2) {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
     }
 }
