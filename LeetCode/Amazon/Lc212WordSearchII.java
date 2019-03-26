74f136744517d454770bd5882225d2daa28ce240
hongyuan_weltond <ningh@umich.edu>
2019-03-26 01:22:10 -0400

    Update Lc212WordSearchII.java

--- LeetCode/Amazon/Lc212WordSearchII.java
+++ LeetCode/Amazon/Lc212WordSearchII.java
@@ -1,100 +1,159 @@
 // https://leetcode.com/problems/word-search-ii/
 
+// ======================== Backtracking + Trie =========================
+// 10ms(99.77%)
 class Solution {
-    int[] dirX = {0, 1, 0, -1};
-    int[] dirY = {1, 0, -1, 0};
     public List<String> findWords(char[][] board, String[] words) {
+        if (board == null || board.length == 0 || words == null || words.length == 0) return new ArrayList();
+        
+        Trie root = new Trie();
+        
+        //Set<String> set = new HashSet();    // to speed up search process
+        
+        // build trie;
+        for (int i = 0; i < words.length; i++) {
+            root.insert(words[i]);
+            //set.add(words);
+        }
+        
         List<String> res = new ArrayList();
         
-        if (board == null || words == null || words.length == 0) return res;
-        
-        TrieNode root = new TrieNode();
-        
-        buildTrie(root, words);
-        //print(root);
-        
-        int n = board.length;
-        int m = board[0].length;
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                bt(board, i, j, root, res);
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                backtracking(board, i, j, root, res);
             }
         }
-
+        
         return res;
     }
     
-    private void bt(char[][] board, int x, int y, TrieNode root, List<String> res) {
+    private void backtracking(char[][] board, int x, int y, Trie root, List<String> res) {
         if (x >= board.length || y >= board[0].length || x < 0 || y < 0) {
             return;
         }
         
         char c = board[x][y];
-        int index = c - 'a';
         
-        if (c == '#' || root.children[index] == null) return;
-        
-        
-        TrieNode p = root.children[index];
-
-        if (p != null && p.word != null) {
-            res.add(p.word);
-            p.word = null;  // avoid duplicate
-            //return;   // !!! Notice: We don't return here because it may only be a prefix of other words.
+        // base case: return directly if c is visited or no trie node
+        if (c == '#' || root.children[c - 'a'] == null) return;
+        // base case: add to res if found
+        Trie tmp = root.children[c - 'a'];
+        if (tmp != null && tmp.word != null) {
+            res.add(tmp.word);
+            tmp.word = null;    // set to null to avoid udplicate
+            // we are not going to return here because we want to keep digging the Trie
         }
         
-        // we are not using extra m * n boolean[][] to optimize space complexity. Notice it also works if you use visited[][] 
-        //visited[x][y] = true;
-        board[x][y] = '#';  
+        board[x][y] = '#';  // mark as visited
+        int[] dir = {0, 1, 0, -1, 0};
+        
+        // 4 way search
         for (int i = 0; i < 4; i++) {
-            bt(board, x + dirX[i], y + dirY[i], p, res);
-        }
-        board[x][y] = c;
-        //visited[x][y] = false;
-    }
-    
-    public void buildTrie(TrieNode root, String[] words) {
-        for (String word : words) {
-            insert(root, word);    
-        }
-    }
-    
-    public void insert(TrieNode root, String key) {
-        int len = key.length();
-        TrieNode dummy = root;
-        int index = 0;
-        for (int i = 0; i < len; i++) {
-            index = key.charAt(i) - 'a';
-            if (dummy.children[index] == null) {
-                dummy.children[index] = new TrieNode();
-            }
-            dummy = dummy.children[index];
+            int nx = x + dir[i];
+            int ny = y + dir[i + 1];
+            
+            backtracking(board, nx, ny, tmp, res);
         }
         
-        dummy.word = key;
+        board[x][y] = c;    // backtracking
     }
     
-    // public void print(TrieNode root) {
-    //     if (root.word != null) {
-    //         System.out.println(root.word);
-    //         return;
-    //     }
-    //     for (int i = 0; i < 26; i++) {
-    //         if (root.children[i] != null) {
-    //             print(root.children[i]);
-    //         }
-    //     }
-    // }
-}
-
-class TrieNode {
-    String word;
-    TrieNode[] children = new TrieNode[26];
-    
-    public TrieNode() {
-        for (int i = 0; i < 26; i++) {
-            children[i] = null;
+    class Trie {
+        Trie[] children;
+        String word;
+        Trie() {
+            children = new Trie[26];
+            word = null;
+        }
+        // If not present, inserts key into Trie
+        // If the key is prefix of Trie node, just marks leaf node
+        public void insert(String s) {
+            int len = s.length();
+            int idx = 0;
+            Trie tmp = this;
+            
+            for (int i = 0; i < len; i++) {
+                idx = s.charAt(i) - 'a';
+                
+                if (tmp.children[idx] == null) {
+                    tmp.children[idx] = new Trie();
+                }
+                tmp = tmp.children[idx];
+            }
+            
+            tmp.word = s;
+        }
+        
+        public boolean search(String s) {
+            int len = s.length();
+            int idx = 0;
+            Trie tmp = this;
+            
+            for (int i = 0; i < len; i++) {
+                idx = s.charAt(i) - 'a';
+                if (tmp.children[idx] == null) return false;
+                
+                tmp = tmp.children[idx];
+            }
+            
+            return tmp != null && tmp.word != null;
+        }
+        
+        public boolean isEmpty(Trie root) {
+            for (int i = 0; i < 26; i++) {
+                if (root.children[i] != null)
+                    return false;
+            }
+            return true;
+        }
+        
+        public Trie delete(Trie root, String s) {
+            return delete(root, s, 0);
+        }
+        
+        // recursive to delete a string from given trie
+        public Trie delete(Trie root, String s, int lvl) {
+            if (root == null) return null;
+            
+            // If last character of key is being processed
+            if (lvl == s.length()) {
+                // This node is no more end of word after removal of given key
+                if (root.word != null) {
+                    root.word = null;
+                }
+                
+                // If given is not prefix of any other word
+                if (isEmpty(root)) {
+                    root = null;
+                }
+                
+                return root;
+            }
+            
+            // If not last character, recur for the child obtained using ASCII value
+            int idx = s.charAt(lvl) - 'a';
+            
+            root.children[idx] = delete(root, s, lvl + 1);
+            
+            // If root deos not have any child (its only child got deleted),
+            // and it is not end of another word
+            if (isEmpty(root) && root.word == null) {
+                root = null;
+            }
+            
+            return root;
+        }
+        
+        public void print(Trie root) {
+            if (root.word != null) {
+                System.out.println(root.word);
+                return;
+            }
+            for (int i = 0; i < 26; i++) {
+                if (root.children[i] != null) {
+                    print(root.children[i]);
+                }
+            }
         }
     }
 }
