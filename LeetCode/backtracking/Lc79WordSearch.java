40e256f1498b9ab30514be092983a9e3a8953d31
hongyuan_weltond <ningh@umich.edu>
2019-03-25 23:01:55 -0400

    Update Lc79WordSearch.java

--- LeetCode/backtracking/Lc79WordSearch.java
+++ LeetCode/backtracking/Lc79WordSearch.java
@@ -17,7 +17,55 @@ Given word = "SEE", return true.
 Given word = "ABCB", return false.
 */
 
+// DFS backtracking
 
+// Approach 2: 4ms (100%)
+class Solution {
+    
+    public boolean exist(char[][] board, String word) {
+        if (board == null || board.length == 0 || word == null || word.length() == 0) return false;
+        
+        char c = word.charAt(0);
+        
+        boolean flag = false;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == c) {
+                    flag = flag || dfs(board, i, j, word, 0);
+                }
+            }
+        }
+        
+        return flag;
+    }
+    
+    private boolean dfs(char[][] board, int x, int y, String word, int lvl) {
+        if (x < 0 || y < 0 || x >= board.length || y >= board[0].length || board[x][y] == '1') return false;
+        if (word.charAt(lvl) != board[x][y]) return false;
+
+        if (lvl + 1 == word.length()) {
+            return true;
+        }
+        
+        char tmp = board[x][y];
+        board[x][y] = '1';
+        
+        int[] dir = {-1, 0, 1, 0, -1};
+        
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dir[i];
+            int ny = y + dir[i + 1];
+            
+            if (dfs(board, nx, ny, word, lvl + 1)) return true;
+        }
+        
+        board[x][y] = tmp;
+        
+        return false;
+    }
+}
+
+// Approach 1 : 9ms
 class Solution {
     public boolean exist(char[][] board, String word) {
         if (board == null || word == null || board.length == 0 || word.length() == 0) {
