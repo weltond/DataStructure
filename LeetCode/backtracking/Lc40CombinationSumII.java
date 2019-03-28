fabf7fcba2798d4a06980c4e01eaac965e67787a
hongyuan_weltond <ningh@umich.edu>
2019-03-28 10:17:43 -0400

    Update Lc40CombinationSumII.java

--- LeetCode/backtracking/Lc40CombinationSumII.java
+++ LeetCode/backtracking/Lc40CombinationSumII.java
@@ -1,6 +1,16 @@
 // https://leetcode.com/problems/combination-sum-ii/
 
 class Solution {
+    /**
+    Input: candidates = [10,1,2,7,6,1,5], target = 8,
+    A solution set is:
+    [
+        [1, 7],
+        [1, 2, 5],
+        [2, 6],
+        [1, 1, 6]
+    ]
+    */
     // ============ Backtracking ==========
     // 4ms (99.74%)
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
