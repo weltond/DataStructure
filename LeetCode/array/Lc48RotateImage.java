97424cd3b80404f9bb30612a7e1b604aeb00a3a9
hongyuan_weltond <ningh@umich.edu>
2019-04-01 10:19:43 -0400

    Update Lc48RotateImage.java

--- LeetCode/array/Lc48RotateImage.java
+++ LeetCode/array/Lc48RotateImage.java
@@ -1,6 +1,44 @@
 // https://leetcode.com/problems/rotate-image/
 
 class Solution {
+    // ============= Iteration ==================
+    // 0ms
+    public void rotate(int[][] matrix) {
+        if (matrix == null || matrix.length == 0) return;
+        /**
+        [
+          [1,2,3],              [9,6,3]
+          [4,5,6],      ===>    [8,5,2]
+          [7,8,9]               [7,4,1]
+        ],
+        */
+        int len = matrix.length;
+        // swap diag
+        for (int i = 0; i < len; i++) {         // row
+            for (int j = 0; j < len - i; j++) { // col
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[len - 1 - j][len - 1 - i];
+                matrix[len - 1 - j][len - 1 - i] = tmp;
+            }
+        }
+        /**
+        [
+          [9,6,3],              [7,4,1]
+          [8,5,2],      ===>    [8,5,2]
+          [7,4,1]               [9,6,3]
+        ],
+        */
+        // swap top and bottom
+        for (int i = 0; i < len; i++) {         // col
+            for (int j = 0; j < len / 2; j++) { // row
+                int tmp = matrix[j][i];
+                matrix[j][i] = matrix[len - 1 - j][i];
+                matrix[len - 1 - j][i] = tmp;
+            }
+        }
+    }
+    
+    // ============= Recursion ==================
     // 0ms (100%)
     public void rotate(int[][] matrix) {
         if (matrix == null || matrix.length == 0) return;
