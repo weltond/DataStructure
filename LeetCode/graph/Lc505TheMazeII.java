850dde81ba4c9297b2af09946cbb3342c206daa5
hongyuan_weltond <ningh@umich.edu>
2019-04-04 16:46:53 -0400

    Create Lc505TheMazeII.java

---
+++ LeetCode/graph/Lc505TheMazeII.java
@@ -0,0 +1,136 @@
+// 
+
+/**
+Input 1: a maze represented by a 2D array
+
+0 0 1 0 0
+0 0 0 0 0
+0 0 0 1 0
+1 1 0 1 1
+0 0 0 0 0
+
+Input 2: start coordinate (rowStart, colStart) = (0, 4)
+Input 3: destination coordinate (rowDest, colDest) = (4, 4)
+
+Output: 12
+
+Explanation: One shortest way is : left -> down -> left -> down -> right -> down -> right.
+             The total distance is 1 + 1 + 3 + 1 + 2 + 2 + 2 = 12.
+             
+             
+Input 1: a maze represented by a 2D array
+
+0 0 1 0 0
+0 0 0 0 0
+0 0 0 1 0
+1 1 0 1 1
+0 0 0 0 0
+
+Input 2: start coordinate (rowStart, colStart) = (0, 4)
+Input 3: destination coordinate (rowDest, colDest) = (3, 2)
+
+Output: -1
+
+Explanation: There is no way for the ball to stop at the destination. 
+*/
+
+// ==================== Method 3: Dijkstra ==========================
+// Time = O(mnlog(mn)), space = O(mn)
+public class Solution {
+    public int shortestDistance(int[][] maze, int[] start, int[] dest) {
+        int[][] distance = new int[maze.length][maze[0].length];
+        for (int[] row: distance)
+            Arrays.fill(row, Integer.MAX_VALUE);
+        distance[start[0]][start[1]] = 0;
+        dijkstra(maze, start, distance);
+        return distance[dest[0]][dest[1]] == Integer.MAX_VALUE ? -1 : distance[dest[0]][dest[1]];
+    }
+
+    public void dijkstra(int[][] maze, int[] start, int[][] distance) {
+        int[][] dirs={{0,1},{0,-1},{-1,0},{1,0}};
+        PriorityQueue < int[] > queue = new PriorityQueue < > ((a, b) -> a[2] - b[2]);
+        queue.offer(new int[]{start[0],start[1],0});
+        while (!queue.isEmpty()) {
+            int[] s = queue.poll();
+            if(distance[s[0]][s[1]] < s[2])
+                continue;
+            for (int[] dir: dirs) {
+                int x = s[0] + dir[0];
+                int y = s[1] + dir[1];
+                int count = 0;
+                while (x >= 0 && y >= 0 && x < maze.length && y < maze[0].length && maze[x][y] == 0) {
+                    x += dir[0];
+                    y += dir[1];
+                    count++;
+                }
+                if (distance[s[0]][s[1]] + count < distance[x - dir[0]][y - dir[1]]) {
+                    distance[x - dir[0]][y - dir[1]] = distance[s[0]][s[1]] + count;
+                    queue.offer(new int[]{x - dir[0], y - dir[1], distance[x - dir[0]][y - dir[1]]});
+                }
+            }
+        }
+    }
+}
+
+// ==================== Method 2: BFS ====================
+// Time O(mn*max(m,n)), space = O(mn)
+public class Solution {
+    public int shortestDistance(int[][] maze, int[] start, int[] dest) {
+        int[][] distance = new int[maze.length][maze[0].length];
+        for (int[] row: distance)
+            Arrays.fill(row, Integer.MAX_VALUE);
+        distance[start[0]][start[1]] = 0;
+         int[][] dirs={{0, 1} ,{0, -1}, {-1, 0}, {1, 0}};
+        Queue < int[] > queue = new LinkedList < > ();
+        queue.add(start);
+        while (!queue.isEmpty()) {
+            int[] s = queue.remove();
+            for (int[] dir: dirs) {
+                int x = s[0] + dir[0];
+                int y = s[1] + dir[1];
+                int count = 0;
+                while (x >= 0 && y >= 0 && x < maze.length && y < maze[0].length && maze[x][y] == 0) {
+                    x += dir[0];
+                    y += dir[1];
+                    count++;
+                }
+                if (distance[s[0]][s[1]] + count < distance[x - dir[0]][y - dir[1]]) {
+                    distance[x - dir[0]][y - dir[1]] = distance[s[0]][s[1]] + count;
+                    queue.add(new int[] {x - dir[0], y - dir[1]});
+                }
+            }
+        }
+        return distance[dest[0]][dest[1]] == Integer.MAX_VALUE ? -1 : distance[dest[0]][dest[1]];
+    }
+}
+
+// ==================== Method 1: DFS ====================
+// Time = O(mn*max(n,m)), space = O(mn)
+public class Solution {
+    public int shortestDistance(int[][] maze, int[] start, int[] dest) {
+        int[][] distance = new int[maze.length][maze[0].length];
+        for (int[] row: distance)
+            Arrays.fill(row, Integer.MAX_VALUE);
+        distance[start[0]][start[1]] = 0;
+        dfs(maze, start, distance);
+        return distance[dest[0]][dest[1]] == Integer.MAX_VALUE ? -1 : distance[dest[0]][dest[1]];
+    }
+
+    public void dfs(int[][] maze, int[] start, int[][] distance) {
+        int[][] dirs={{0,1}, {0,-1}, {-1,0}, {1,0}};
+        for (int[] dir: dirs) {
+            int x = start[0] + dir[0];
+            int y = start[1] + dir[1];
+            int count = 0;
+            while (x >= 0 && y >= 0 && x < maze.length && y < maze[0].length && maze[x][y] == 0) {
+                x += dir[0];
+                y += dir[1];
+                count++;
+            }
+            if (distance[start[0]][start[1]] + count < distance[x - dir[0]][y - dir[1]]) {
+                distance[x - dir[0]][y - dir[1]] = distance[start[0]][start[1]] + count;
+                dfs(maze, new int[]{x - dir[0],y - dir[1]}, distance);
+            }
+        }
+    }
+}
