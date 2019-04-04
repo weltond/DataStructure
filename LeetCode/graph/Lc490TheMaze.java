b5336955e4b1e450093b989fa61cfe355c0620db
hongyuan_weltond <ningh@umich.edu>
2019-04-04 15:50:52 -0400

    Update Lc490TheMaze.java

--- LeetCode/graph/Lc490TheMaze.java
+++ LeetCode/graph/Lc490TheMaze.java
@@ -31,6 +31,34 @@ public class Solution {
 
 // =============== Method 1: DFS ======================
 // TLE
+// Approach 2
+class Solution {
+    private int[][] dir = new int[][]{{-1,0},{1,0},{0,-1},{0,1}};
+    
+    public boolean hasPath(int[][] maze, int[] start, int[] destination) {
+        boolean[][] visited = new boolean[maze.length][maze[0].length];
+        return dfs(maze, visited, start, destination);
+    }
+    
+    private boolean dfs(int[][] maze, boolean[][] visited, int[] c, int[] des) {
+        if (visited[c[0]][c[1]]) return false;
+        if (c[0] == des[0] && c[1] == des[1]) return true;
+        
+        visited[c[0]][c[1]] = true;
+        boolean result = false;
+        for (int[] d : dir) {
+            int x = c[0] + d[0];
+            int y = c[1] + d[1];
+            while ( 0 <= x && x < maze.length && 0 <= y && y < maze[0].length && maze[x][y] == 0) {
+                x += d[0];
+                y += d[1];
+            }
+            result = result || dfs(maze, visited, new int[]{ x - d[0], y - d[1]}, des);
+        }
+        return result;
+    }
+}
+// Approach 1
 public class Solution {
     public boolean hasPath(int[][] maze, int[] start, int[] destination) {
         boolean[][] visited = new boolean[maze.length][maze[0].length];
