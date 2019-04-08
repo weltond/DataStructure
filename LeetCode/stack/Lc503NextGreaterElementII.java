286846eb7aa051390b0a19bf1b8031bf667ef668
hongyuan_weltond <ningh@umich.edu>
2019-04-08 00:31:15 -0400

    Update Lc503NextGreaterElementII.java

--- LeetCode/stack/Lc503NextGreaterElementII.java
+++ LeetCode/stack/Lc503NextGreaterElementII.java
@@ -3,6 +3,22 @@
 class Solution {
     // ============== Method 1: Monotonic Stack =================
     // 11ms (96.96%)
+    // Approach 2
+    public int[] nextGreaterElements(int[] nums) {
+        int[] res = new int[nums.length];
+
+        Deque<Integer> stack = new LinkedList();
+        for (int i = 2 * nums.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && stack.peek() <= nums[i % nums.length]) {
+                stack.pop();
+            }
+            res[i % nums.length] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(nums[i % nums.length]);
+        }
+        return res;
+    }
+    
+    // Approach 1
     public int[] nextGreaterElements(int[] nums) {
         int[] res = new int[nums.length];
         int[] tmp = new int[nums.length * 2];
