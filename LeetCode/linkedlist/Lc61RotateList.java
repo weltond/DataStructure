47a9434e6dff1b38f99841a03774ac5759539631
hongyuan_weltond <ningh@umich.edu>
2019-04-09 11:15:40 -0400

    Update Lc61RotateList.java

--- LeetCode/linkedlist/Lc61RotateList.java
+++ LeetCode/linkedlist/Lc61RotateList.java
@@ -8,6 +8,21 @@
  *     ListNode(int x) { val = x; }
  * }
  */
+/**
+Input: 1->2->3->4->5->NULL, k = 2
+Output: 4->5->1->2->3->NULL
+Explanation:
+rotate 1 steps to the right: 5->1->2->3->4->NULL
+rotate 2 steps to the right: 4->5->1->2->3->NULL
+
+Input: 0->1->2->NULL, k = 4
+Output: 2->0->1->NULL
+Explanation:
+rotate 1 steps to the right: 2->0->1->NULL
+rotate 2 steps to the right: 1->2->0->NULL
+rotate 3 steps to the right: 0->1->2->NULL
+rotate 4 steps to the right: 2->0->1->NULL
+*/
 class Solution {
     // ============ Method 2 ===============
     // 0ms
