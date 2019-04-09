6edbda3cefa1ac1805be66915826b170154ee230
hongyuan_weltond <ningh@umich.edu>
2019-04-09 11:15:03 -0400

    Update Lc725SplitLinkedListsinParts.java

--- LeetCode/linkedlist/Lc725SplitLinkedListsinParts.java
+++ LeetCode/linkedlist/Lc725SplitLinkedListsinParts.java
@@ -8,6 +8,24 @@
  *     ListNode(int x) { val = x; }
  * }
  */
+
+/**
+Input: 
+root = [1, 2, 3], k = 5
+Output: [[1],[2],[3],[],[]]
+Explanation:
+The input and each element of the output are ListNodes, not arrays.
+For example, the input root has root.val = 1, root.next.val = 2, \root.next.next.val = 3, and root.next.next.next = null.
+The first element output[0] has output[0].val = 1, output[0].next = null.
+The last element output[4] is null, but it's string representation as a ListNode is [].
+
+Input: 
+root = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], k = 3
+Output: [[1, 2, 3, 4], [5, 6, 7], [8, 9, 10]]
+Explanation:
+The input has been split into consecutive parts with size difference at most 1, 
+and earlier parts are a larger size than the later parts.
+*/
 class Solution {
     // 0ms. Time = O(N + k), Space = O(k) or O(1)
     public ListNode[] splitListToParts(ListNode root, int k) {
