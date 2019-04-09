97a3b0d4a984be6d9e5da1ae7c8b5a6f86f5ec95
hongyuan_weltond <ningh@umich.edu>
2019-04-09 09:35:14 -0400

    Create Lc1019NextGreaterNodeInLinkedList.java

---
+++ LeetCode/stack/Lc1019NextGreaterNodeInLinkedList.java
@@ -0,0 +1,62 @@
+// https://leetcode.com/problems/next-greater-node-in-linked-list/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    int[] res;
+    // =========== Method 2: Recursion ===========
+    // 3ms (100%)
+    public int[] nextLargerNodes(ListNode head) {
+        if (head == null) return new int[0];
+        helper(head, 0);  
+        return res;
+    }
+    
+    private ListNode helper(ListNode head, int length) {
+        if (head == null) {
+            res = new int[length];
+            return null;
+        }
+        
+        ListNode next = helper(head.next, length + 1);
+        
+        while (next != null && head.val >= next.val) {
+            next = next.next;
+        }
+        if (next != null) {
+            res[length] = next.val;
+        }
+        
+        head.next = next;     // without this line, runtime is 1390ms (5.05%)
+        return head;
+    }
+    // =========== Method 1: Iteration ===========
+    // 15ms (91.90%)
+//     public int[] nextLargerNodes(ListNode head) {
+//         int[] tmp = new int[10001];
+//         int i = 0;
+//         while (head != null) {
+//             tmp[i++] = head.val;
+//             head = head.next;
+//         }
+        
+//         int[] res = new int[i];
+        
+//         Deque<Integer> stack = new LinkedList();
+//         for (int j = i - 1; j >= 0; j--) {
+//             while (!stack.isEmpty() && tmp[j] >= stack.peek()) {
+//                 stack.pop();
+//             }
+//             res[j] = stack.isEmpty() ? 0 : stack.peek();
+//             stack.push(tmp[j]);
+//         }
+        
+//         return res;
+//     }
+}
