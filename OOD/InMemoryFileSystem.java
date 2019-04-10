315d74498c397e8bcfaa5aa3a20a3e46fc1e7b67
hongyuan_weltond <ningh@umich.edu>
2019-04-09 23:10:41 -0400

    Create InMemoryFileSystem.java

---
+++ OOD/InMemoryFileSystem.java
@@ -0,0 +1,9 @@
+/**
+A file System, in its most simplistic version, consists of Files and Directories. 
+Each Directory contains a set of Files and Directories. Since Files and Directories share so many characteristics,
+we can implement them such that they inherit from the same class, Entry.
+*/
+
+public abstract class Entry {
+  
+}
