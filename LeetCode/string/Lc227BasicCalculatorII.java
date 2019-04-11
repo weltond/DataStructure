5a7daf87409ca80366398877bc5fb47082ab90ba
hongyuan_weltond <ningh@umich.edu>
2019-04-11 09:32:26 -0400

    Update Lc227BasicCalculatorII.java

--- LeetCode/string/Lc227BasicCalculatorII.java
+++ LeetCode/string/Lc227BasicCalculatorII.java
@@ -1,4 +1,73 @@
-// 
+// https://leetcode.com/problems/basic-calculator-ii/
+
+/**
+Input: "3+2*2"
+Output: 7
+*/
+// ====================== Method 2 ========================
+// 7ms (97.22%)
+class Solution {
+    
+    public int calculate(String s) {
+        int len = s.length(), res = 0, mul = 1, tmp = 0;
+        boolean isMul = true, isAdd = true;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            
+            if (c <= '9' && c >= '0') {
+                tmp = c - '0';
+                while (i + 1 < len && Character.isDigit(s.charAt(i + 1))) {
+                    tmp = tmp * 10 + s.charAt(i + 1) - '0';
+                    i++;
+                }
+                continue;
+            }
+            if (c == ' ') continue;
+            mul = isMul ? mul * tmp : mul / tmp;
+            if (c == '+' || c == '-') {
+                res = isAdd ? res + mul : res - mul;    //last + or -
+                tmp = 0;
+                mul = 1;
+                isMul = true;
+                isAdd = c == '+';   // current + or -
+            } else if (c == '*' || c == '/') {
+                tmp = 0;
+                isMul = c == '*';
+            }
+        }
+        mul = isMul ? mul * tmp : mul / tmp;
+        
+        return isAdd ? res + mul : res - mul;
+    }
+    
+    // public OP getOp(char c) {
+    //     switch c:
+    //     case '+':
+    //     return OP.ADD;
+    //     case '-':
+    //     return OP.MINUS;
+    //     case '*':
+    //     return OP.MULTI;
+    //     case '/':
+    //     return OP.DIV;
+    // }
+}
+
+// enum OP {
+//     ADD('+', 0), MINUS('-', 0), MULTI('*', 1), DIV('/', 1);
+//     private char c;
+//     private int lvl;
+//     OP(char c, int lvl) {
+//         this.c = c;
+//         this.lvl = lvl;
+//     }
+//     public int getLvl() {
+//         return lvl;
+//     }
+//     public char getVal() {
+//         return c;
+//     }
+// }
 
 // ====================== Method 1 =====================
 // 78ms (5.80%)
