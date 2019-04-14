ec9e7475e293fa833ad7ec3e3399c3da07d7fa7b
hongyuan_weltond <ningh@umich.edu>
2019-04-14 13:07:50 -0400

    Update Lc117PopNextRightPointersII.java

--- LeetCode/tree/Lc117PopNextRightPointersII.java
+++ LeetCode/tree/Lc117PopNextRightPointersII.java
@@ -18,6 +18,66 @@ class Node {
     }
 };
 */
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val,Node _left,Node _right,Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+class Solution {
+    // ========== Method 1: DFS ============
+    // 0ms (100%)
+    public Node connect(Node root) {
+        if (root == null) return null;
+        
+        if (root.left != null) {
+            if (root.right != null) {
+                root.left.next = root.right;
+            } else {
+                root.left.next = findNextToConnect(root.next);
+            }
+        }
+        if (root.right != null) {
+            root.right.next = findNextToConnect(root.next);
+        }
+        
+        /**
+               1  ->   2
+             /  \     / \
+            3 -> 4 ->5   6
+           /              \
+         7                 8
+         Consider above. If recurse left first, 7 can not find next node.
+        */
+        connect(root.right);    
+        connect(root.left);
+        
+        return root;
+    }
+    private Node findNextToConnect(Node root) {
+        if (root == null) return null;
+        
+        if (root.left != null) return root.left;
+        if (root.right != null) return root.right;
+        
+        return findNextToConnect(root.next);
+        
+    }
+}
+
 class Solution {
     // ======== Method 1: BFS =============
     public Node connect(Node root) {
