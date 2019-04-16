7e70e52fe501c31eb14bf1117331b798fae51048
hongyuan_weltond <ningh@umich.edu>
2019-04-16 11:15:18 -0400

    Create Lc12IntegerToRoman.java

---
+++ LeetCode/string/Lc12IntegerToRoman.java
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/integer-to-roman/
+
+/**
+Input: 1994
+Output: "MCMXCIV"
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+*/
+class Solution {
+    // ========== Method 2 ===========
+    // 3ms (100%)
+    public String intToRoman(int num) {
+        int[] values = new int[]{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] romans = new String[]{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        
+        StringBuilder result = new StringBuilder();
+        
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                num -= values[i];
+                result.append(romans[i]);
+            }
+        }
+        
+        return result.toString();
+    }
+    // ============ Method 1 =============
+    // 4ms (88.89%)
+    public String intToRoman(int num) {
+        String M[] = {"", "M", "MM", "MMM"};
+        String C[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        String X[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        String I[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        
+        return M[num / 1000] + C[(num % 1000) / 100] + X[(num % 100) / 10] + I[(num % 10)];
+    }
+}
