704b0160704a815891a96ee47118d9789be3ca75
hongyuan_weltond <ningh@umich.edu>
2019-04-16 19:37:05 -0400

    Create Lc273IntegerToEnglishWords.java

---
+++ LeetCode/string/Lc273IntegerToEnglishWords.java
@@ -0,0 +1,50 @@
+// https://leetcode.com/problems/integer-to-english-words/
+
+/**
+Example 1:
+
+Input: 123
+Output: "One Hundred Twenty Three"
+Example 2:
+
+Input: 12345
+Output: "Twelve Thousand Three Hundred Forty Five"
+Example 3:
+
+Input: 1234567
+Output: "One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven"
+Example 4:
+
+Input: 1234567891
+Output: "One Billion Two Hundred Thirty Four Million Five Hundred Sixty Seven Thousand Eight Hundred Ninety One"
+*/
+class Solution {
+    // ========== Method 1 ===========
+    // 2ms (51.46%)
+    final String[] LESS_THAN_20 = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    final String[] TENS = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    final String[] THOUSANDS = {"", "Thousand", "Million", "Billion"};
+    public String numberToWords(int num) {
+        if (num == 0) return "Zero";
+
+        int i = 0;
+        String words = "";
+        
+        while (num > 0) {
+            if (num % 1000 != 0) {
+                words = helper(num % 1000) + THOUSANDS[i] + " " + words;
+            }
+            num = num / 1000;
+            i++;
+        }
+        
+        return words.trim();
+    }
+    
+    private String helper(int num) {
+        if (num == 0) return "";
+        else if (num < 20) return LESS_THAN_20[num] + " ";
+        else if (num < 100) return TENS[num / 10] + " " + helper(num % 10);
+        else return LESS_THAN_20[num / 100] + " Hundred " + helper(num % 100);
+    }
+}
