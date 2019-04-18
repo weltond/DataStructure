da50777e4fcaed111e1a4e3a00b6017247099ef8
hongyuan_weltond <ningh@umich.edu>
2019-04-18 17:30:46 -0400

    Update Lc81SearchInRotatedSortedArrayII.java

--- LeetCode/search/binarysearch/Lc81SearchInRotatedSortedArrayII.java
+++ LeetCode/search/binarysearch/Lc81SearchInRotatedSortedArrayII.java
@@ -12,6 +12,38 @@
 class Solution {
     public boolean search(int[] nums, int target) {
         if (nums == null || nums.length == 0) return false;
+        int left = 0;
+        int right = nums.length - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] == target || nums[left] == target || nums[right] == target) return true;
+            
+            if (nums[mid] > nums[right]) {  // left is sorted
+                if (nums[mid] > target && nums[left] <= target) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else if (nums[mid] < nums[right]) {   // right is sorted
+                if (nums[mid] < target && nums[right] >= target) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            } else {
+                left++;
+            }
+        }
+        
+        return false;
+    }
+}
+
+class Solution {
+    public boolean search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return false;
         
         int left = 0;
         int right = nums.length - 1;
