deb05de8065af11751d7c9ca2dcb33322a71f406
hongyuan_weltond <ningh@umich.edu>
2019-04-17 23:24:24 -0400

    Update Lc62UniquePaths.java

--- LeetCode/dp/Lc62UniquePaths.java
+++ LeetCode/dp/Lc62UniquePaths.java
@@ -1,5 +1,20 @@
 // https://leetcode.com/problems/unique-paths/
 
+/**
+Example 1:
+
+Input: m = 3, n = 2
+Output: 3
+Explanation:
+From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+1. Right -> Right -> Down
+2. Right -> Down -> Right
+3. Down -> Right -> Right
+Example 2:
+
+Input: m = 7, n = 3
+Output: 28
+*/
 class Solution {
     // ========== Method 2: DP ============
     // 0ms
