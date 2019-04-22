028b0862ef88e87046408251098ddeebd3e85142
hongyuan_weltond <ningh@umich.edu>
2019-04-21 21:05:00 -0400

    Update Lc350IntersectionOfTwoArraysII.java

--- LeetCode/array/Lc350IntersectionOfTwoArraysII.java
+++ LeetCode/array/Lc350IntersectionOfTwoArraysII.java
@@ -3,14 +3,17 @@
 
 /**
 Follow up:
-
-What if the given array is already sorted? How would you optimize your algorithm?
-What if nums1's size is small compared to nums2's size? Which algorithm is better?
 What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
 - If only nums2 cannot fit in memory, put all elements of nums1 into a HashMap, 
 read chunks of array that fit into the memory, and record the intersections.
 - If both nums1 and nums2 are so huge that neither fit into the memory, 
 sort them individually (external sort), then read 2 elements from each array at a time in memory, record intersections.
+=============================
+1. Store the two strings in distributed system(whether self designed or not), then using MapReduce technique to solve the problem;
+
+2. Processing the Strings by chunk, which fits the memory, then deal with each chunk of data at a time;
+
+3. Processing the Strings by streaming, then check.
 */
 
 class Solution {
