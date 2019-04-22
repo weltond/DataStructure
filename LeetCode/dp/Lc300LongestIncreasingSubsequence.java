370c87c3260c94fac14ad82e7088a16035427161
hongyuan_weltond <ningh@umich.edu>
2019-04-22 17:29:30 -0400

    Create Lc300LongestIncreasingSubsequence.java

---
+++ LeetCode/dp/Lc300LongestIncreasingSubsequence.java
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/longest-increasing-subsequence/
+
+/**
+Input: [10,9,2,5,3,7,101,18]
+Output: 4 
+Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4. 
+*/
+
+class Solution {
+    // ============= Method 1: DP ================
+    // 8ms (65.83%)
+    public int lengthOfLIS(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+        int res = 1;
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            
+            res = Math.max(res, dp[i]);
+        }
+        
+        return res;
+    }
+}
