d97823353e42cea6b13f1e0600d8946b57e30505
hongyuan_weltond <ningh@umich.edu>
2019-04-21 21:03:01 -0400

    Update Lc350IntersectionOfTwoArraysII.java

--- LeetCode/array/Lc350IntersectionOfTwoArraysII.java
+++ LeetCode/array/Lc350IntersectionOfTwoArraysII.java
@@ -1,5 +1,18 @@
 // https://leetcode.com/problems/intersection-of-two-arrays-ii/
 
+
+/**
+Follow up:
+
+What if the given array is already sorted? How would you optimize your algorithm?
+What if nums1's size is small compared to nums2's size? Which algorithm is better?
+What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
+- If only nums2 cannot fit in memory, put all elements of nums1 into a HashMap, 
+read chunks of array that fit into the memory, and record the intersections.
+- If both nums1 and nums2 are so huge that neither fit into the memory, 
+sort them individually (external sort), then read 2 elements from each array at a time in memory, record intersections.
+*/
+
 class Solution {
     // ========= Method 2: Two Pointer ==========
     public int[] intersect(int[] nums1, int[] nums2) {
