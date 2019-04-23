7ce227b130a62e4fe12c11946e4da3976667ad06
hongyuan_weltond <ningh@umich.edu>
2019-04-22 20:35:37 -0400

    Update Lc146LRUCache.java

--- LeetCode/hashmap/Lc146LRUCache.java
+++ LeetCode/hashmap/Lc146LRUCache.java
@@ -1,8 +1,8 @@
 // https://leetcode.com/problems/lru-cache/
 
-/**
-LRUCache cache = new LRUCache( 2 /* capacity */ );
 
+//LRUCache cache = new LRUCache( 2 /* capacity */ );
+/**
 cache.put(1, 1);
 cache.put(2, 2);
 cache.get(1);       // returns 1
@@ -109,105 +109,105 @@ class Node {
     }
 }
 /**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache obj = new LRUCache(capacity);
- * int param_1 = obj.get(key);
- * obj.put(key,value);
- */
+* Your LRUCache object will be instantiated and called as such:
+* LRUCache obj = new LRUCache(capacity);
+* int param_1 = obj.get(key);
+* obj.put(key,value);
+*/
  
  
  
- // 65ms
- class LRUCache {
-    public Map<Integer, Node> map;   // key - Node pair
-    private int cnt;
-    private int cap;
-    private Node dummyHead; // least recent used
-    private Node dummyTail; 
-    
-    public LRUCache(int capacity) {
-        map = new HashMap<>();
-        this.cnt = 0;
-        this.cap = capacity;
-        dummyHead = new Node(0,0);
-        dummyTail = new Node(0,0);
-        dummyHead.next = dummyTail;
-        dummyTail.prev = dummyHead;
-    }
-    
-    public int get(int key) {
-        if (!map.containsKey(key)) return -1;
-        Node cur = map.get(key);
-        
-        remove(cur);
-        
-        insert(cur);
-        
-        return cur.val;
-    }
-    
-    private void remove(Node n) {
-        Node tmp = n.prev;
-        Node tmp2 = n.next;
+// 65ms
+class LRUCache {
+public Map<Integer, Node> map;   // key - Node pair
+private int cnt;
+private int cap;
+private Node dummyHead; // least recent used
+private Node dummyTail; 
 
-        tmp.next = tmp2;
-        tmp2.prev = tmp;
+public LRUCache(int capacity) {
+    map = new HashMap<>();
+    this.cnt = 0;
+    this.cap = capacity;
+    dummyHead = new Node(0,0);
+    dummyTail = new Node(0,0);
+    dummyHead.next = dummyTail;
+    dummyTail.prev = dummyHead;
+}
+
+public int get(int key) {
+    if (!map.containsKey(key)) return -1;
+    Node cur = map.get(key);
+
+    remove(cur);
+
+    insert(cur);
+
+    return cur.val;
+}
+
+private void remove(Node n) {
+    Node tmp = n.prev;
+    Node tmp2 = n.next;
+
+    tmp.next = tmp2;
+    tmp2.prev = tmp;
+}
+
+private void insert(Node n) {
+    Node oldHead = dummyHead.next;
+    n.next = oldHead;
+    n.prev = dummyHead;
+    oldHead.prev = n;
+    dummyHead.next = n;
+}
+
+public void put(int key, int value) {
+    Node n = null;
+
+    if (!map.containsKey(key)) {
+        n = new Node(key, value);
+        map.put(key, n);
+        cnt++;
+    } else {
+        n = map.get(key);
+        n.val = value;
+
+        remove(n);
     }
-    
-    private void insert(Node n) {
-        Node oldHead = dummyHead.next;
-        n.next = oldHead;
-        n.prev = dummyHead;
-        oldHead.prev = n;
-        dummyHead.next = n;
+
+    // put this key to head
+    insert(n);
+
+
+    // if full, remove the last
+    if (cnt > this.cap) {
+        Node oldLast = dummyTail.prev;
+        remove(oldLast);
+
+        map.remove(oldLast.key);
+        cnt--;
     }
-    
-    public void put(int key, int value) {
-        Node n = null;
-        
-        if (!map.containsKey(key)) {
-            n = new Node(key, value);
-            map.put(key, n);
-            cnt++;
-        } else {
-            n = map.get(key);
-            n.val = value;
-            
-            remove(n);
-        }
-        
-        // put this key to head
-        insert(n);
-        
-        
-        // if full, remove the last
-        if (cnt > this.cap) {
-            Node oldLast = dummyTail.prev;
-            remove(oldLast);
-            
-            map.remove(oldLast.key);
-            cnt--;
-        }
-    }
+}
 }
 
 class Node {
-    int key;
-    int val;
-    Node next;
-    Node prev;
-    
-    public Node(int key, int val) {
-        this.key = key;
-        this.val = val;
-        next = null;
-        prev = null;
-    }
+int key;
+int val;
+Node next;
+Node prev;
+
+public Node(int key, int val) {
+    this.key = key;
+    this.val = val;
+    next = null;
+    prev = null;
+}
 }
 
 /**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache obj = new LRUCache(capacity);
- * int param_1 = obj.get(key);
- * obj.put(key,value);
- */
+* Your LRUCache object will be instantiated and called as such:
+* LRUCache obj = new LRUCache(capacity);
+* int param_1 = obj.get(key);
+* obj.put(key,value);
+*/
