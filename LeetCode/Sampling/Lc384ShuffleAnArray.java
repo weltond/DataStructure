6f1bbd8f113e448750e78116b3f9c37d7cdf72d4
hongyuan_weltond <ningh@umich.edu>
2019-04-23 21:17:58 -0400

    Create Lc384ShuffleAnArray.java

---
+++ LeetCode/Sampling/Lc384ShuffleAnArray.java
@@ -0,0 +1,59 @@
+// https://leetcode.com/problems/shuffle-an-array/
+
+/**
+// Init an array with set 1, 2, and 3.
+int[] nums = {1,2,3};
+Solution solution = new Solution(nums);
+
+// Shuffle the array [1,2,3] and return its result. Any permutation of [1,2,3] must equally likely to be returned.
+solution.shuffle();
+
+// Resets the array back to its original configuration [1,2,3].
+solution.reset();
+
+// Returns the random shuffling of array [1,2,3].
+solution.shuffle();*/
+
+class Solution {
+    // 122ms (62.88%)
+    int[] array;
+    int[] original;
+    public Solution(int[] nums) {
+        original = nums.clone();
+        array = nums;
+    }
+    
+    /** Resets the array to its original configuration and return it. */
+    public int[] reset() {
+        array = original;
+        original = original.clone();
+        return original;
+    }
+    
+    /** Returns a random shuffling of the array. */
+    public int[] shuffle() {
+        for (int i = 0; i < array.length; i++) {
+            swapAt(i, randRange(i, array.length));
+        }
+        return array;
+    }
+    
+    Random rand = new Random();
+
+    private int randRange(int min, int max) {
+        return rand.nextInt(max - min) + min;
+    }
+
+    private void swapAt(int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int[] param_1 = obj.reset();
+ * int[] param_2 = obj.shuffle();
+ */
