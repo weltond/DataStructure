8daf516a7bfba9ddd0419f9b17233da4553f619f
hongyuan_weltond <ningh@umich.edu>
2019-04-23 21:19:21 -0400

    Update Lc384ShuffleAnArray.java

--- LeetCode/Sampling/Lc384ShuffleAnArray.java
+++ LeetCode/Sampling/Lc384ShuffleAnArray.java
@@ -15,6 +15,7 @@ solution.reset();
 solution.shuffle();*/
 
 class Solution {
+    // ====== Method 2: Fisher-Yates =======
     // 122ms (62.88%)
     int[] array;
     int[] original;
@@ -57,3 +58,42 @@ class Solution {
  * int[] param_1 = obj.reset();
  * int[] param_2 = obj.shuffle();
  */
+
+// ======== Method 1: Brute Force =======
+class Solution {
+    private int[] array;
+    private int[] original;
+
+    private Random rand = new Random();
+
+    private List<Integer> getArrayCopy() {
+        List<Integer> asList = new ArrayList<Integer>();
+        for (int i = 0; i < array.length; i++) {
+            asList.add(array[i]);
+        }
+        return asList;
+    }
+
+    public Solution(int[] nums) {
+        array = nums;
+        original = nums.clone();
+    }
+    
+    public int[] reset() {
+        array = original;
+        original = original.clone();
+        return array;
+    }
+    
+    public int[] shuffle() {
+        List<Integer> aux = getArrayCopy();
+
+        for (int i = 0; i < array.length; i++) {
+            int removeIdx = rand.nextInt(aux.size());
+            array[i] = aux.get(removeIdx);
+            aux.remove(removeIdx);
+        }
+
+        return array;
+    }
+}
