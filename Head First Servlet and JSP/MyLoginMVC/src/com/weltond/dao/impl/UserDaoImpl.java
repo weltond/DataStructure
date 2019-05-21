d0b88ba694ede288cbb24032ad9f26588cd54f7a
weltond <ningh@umich.edu>
2019-05-21 19:17:26 -0400

    implement Dao

--- Head First Servlet and JSP/MyLoginMVC/src/com/weltond/dao/impl/UserDaoImpl.java
+++ Head First Servlet and JSP/MyLoginMVC/src/com/weltond/dao/impl/UserDaoImpl.java
@@ -2,6 +2,11 @@ package com.weltond.dao.impl;
 
 import com.weltond.dao.UserDao;
 import com.weltond.domain.User;
+import com.weltond.utils.DBUtils;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.text.SimpleDateFormat;
 
 /**
  * @author Weltond Ning
@@ -11,6 +16,25 @@ import com.weltond.domain.User;
 public class UserDaoImpl implements UserDao {
     @Override
     public void addUser(User user) throws Exception {
+        Connection connection = null;
+        PreparedStatement ps = null;
 
+        try {
+            connection = DBUtils.getConnection();
+            ps = connection.prepareStatement("INSERT INTO users(username, password, email, birthday) VALUES(?,?,?,?)");
+            ps.setString(1, user.getUsername());
+            ps.setString(2, user.getPassword());
+            ps.setString(3, user.getEmail());
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            String date = sdf.format(user.getBirthday());
+            ps.setString(4, date);
+
+            int i = ps.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Fail to Add Users!");
+        } finally {
+            DBUtils.closeAll(null, ps, connection);
+        }
     }
 }
--- Head First Servlet and JSP/MyLoginMVC/src/com/weltond/utils/DBUtils.java
+++ Head First Servlet and JSP/MyLoginMVC/src/com/weltond/utils/DBUtils.java
@@ -1,5 +1,11 @@
 package com.weltond.utils;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+
 /**
  * @author Weltond Ning
  * @Project MyLoginMVC
@@ -10,4 +16,50 @@ public class DBUtils {
     private static String url;
     private static String username;
     private static String password;
+
+    static {
+        // load property
+        ResourceBundle rb = ResourceBundle.getBundle("dbinfo");
+        driverClass = rb.getString("driverClass");
+        url = rb.getString("url");
+        username = rb.getString("username");
+        password = rb.getString("password");
+
+        try {
+            Class.forName(driverClass);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() throws Exception {
+        return DriverManager.getConnection(url, username, password);
+    }
+
+    public static void closeAll(ResultSet rs, Statement stmt, Connection connection) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            rs = null;
+        }
+        if (stmt != null) {
+            try {
+                stmt.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            stmt = null;
+        }
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            connection = null;
+        }
+    }
 }
