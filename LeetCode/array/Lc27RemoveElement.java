4f3e6e7654c63048506225fcb711431a5065f51e
hongyuan_weltond <ningh@umich.edu>
2019-07-25 01:46:27 -0400

    Update Lc27RemoveElement.java

--- LeetCode/array/Lc27RemoveElement.java
+++ LeetCode/array/Lc27RemoveElement.java
@@ -9,6 +9,31 @@ Note that the order of those five elements can be arbitrary.
 
 It doesn't matter what values are set beyond the returned length.
 */
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        if (nums == null) return 0;
+        
+        int left = 0, right = nums.length - 1; // excluding
+        
+        while (left <= right) {
+            while (left <= right && nums[left] != val) left++;
+            while (left <= right && nums[right] == val) right--;
+            
+            if (left <= right)
+                swap(nums, left++, right--);
+        }
+        
+        return left;
+    }
+    
+    private void swap(int[] a, int i, int j) {
+        int tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+}
+
 class Solution {
     // 0ms
     public int removeElement(int[] nums, int val) {
