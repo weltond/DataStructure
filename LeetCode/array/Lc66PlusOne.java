b8b547bf22232ad3c02b85602dc1c3846f7560e6
hongyuan_weltond <ningh@umich.edu>
2019-07-29 19:44:36 -0400

    Update Lc66PlusOne.java

--- LeetCode/array/Lc66PlusOne.java
+++ LeetCode/array/Lc66PlusOne.java
@@ -1,4 +1,30 @@
 //https://leetcode.com/problems/plus-one/
+class Solution {
+    public int[] plusOne(int[] digits) {
+        if (digits == null || digits.length == 0) return digits;
+        
+        int f = 0;
+        
+        for (int i = digits.length - 1; i >= 0; i--) {
+            int sum = (i == digits.length - 1 ? 1 : 0) + digits[i] + f;
+            
+            digits[i] = sum % 10;
+            f = sum / 10;
+            if (sum < 10) {
+                return digits;
+            }
+            
+        }
+        
+        int[] arr = new int[digits.length + 1];
+        arr[0] = f;
+        for (int i = 1; i < arr.length; i++) {
+            arr[i] = digits[i - 1];
+        }
+        
+        return arr;
+    }
+}
 
 class Solution {
     public int[] plusOne(int[] digits) {
