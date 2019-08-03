5a566f3168828dbc07b4fb6622809dadc1ce9385
hongyuan_weltond <ningh@umich.edu>
2019-08-02 22:51:24 -0400

    Update Lc92ReverseLinkedListII.java

--- LeetCode/linkedlist/Lc92ReverseLinkedListII.java
+++ LeetCode/linkedlist/Lc92ReverseLinkedListII.java
@@ -14,6 +14,40 @@ Output: 1->4->3->2->5->NULL
  * }
  */
 class Solution {
+    // Like No.25 Reverse Nodes in K Group
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        if (head == null || head.next == null || m == n) return head;
+        
+        ListNode dummy = new ListNode(0);
+        ListNode prev = dummy;
+        dummy.next = head;
+        
+        int diff = n - m;
+        while (m-- > 1) {
+            prev = head;
+            head = head.next;
+        }
+
+        while (diff-- > 0) {
+            ListNode t = head.next.next;
+            head.next.next = prev.next;
+            prev.next = head.next;
+            head.next = t;
+        }
+        
+        return dummy.next;
+    }
+}
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
     // 0ms
     public ListNode reverseBetween(ListNode head, int m, int n) {
         if (head == null) return null;
