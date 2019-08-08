bfcb50cfef7ad8bb93a6d7d5b338638d9e2f48ba
hongyuan_weltond <ningh@umich.edu>
2019-08-08 09:10:09 -0400

    Update Lc109ConvertSortedListToBST.java

--- LeetCode/tree/Lc109ConvertSortedListToBST.java
+++ LeetCode/tree/Lc109ConvertSortedListToBST.java
@@ -1,3 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    // 1ms (100%)
+    public TreeNode sortedListToBST(ListNode head) {
+        if (head == null) return null;
+        if (head.next == null) return new TreeNode(head.val);
+        
+        ListNode node = head;
+        ListNode slow = node;
+        ListNode fast = node, prev = null;
+        while (fast != null && fast.next != null) {
+            fast = fast.next;
+            prev = slow;
+            slow = slow.next;
+            if (fast != null) fast = fast.next;
+        }
+        ListNode mid = prev.next;
+        
+        ListNode next = mid.next;
+        //System.out.println(head.val + ","+mid.val +","+next.val);
+        int val = mid.val;
+        
+        prev.next = null;
+        
+        TreeNode root = new TreeNode(val);
+        root.left = sortedListToBST(head);
+        root.right = sortedListToBST(next);
+        
+        return root;
+    }
+}
+
 class Solution {
     public TreeNode sortedListToBST(ListNode head) {
         
