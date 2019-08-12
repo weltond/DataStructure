b3279ddf6d33e4906c3dcd1530077f45adeaa818
hongyuan_weltond <ningh@umich.edu>
2019-08-12 01:25:46 -0400

    Update Lc119PascalTriangleII.java

--- LeetCode/array/Lc119PascalTriangleII.java
+++ LeetCode/array/Lc119PascalTriangleII.java
@@ -1,4 +1,29 @@
 // https://leetcode.com/problems/pascals-triangle-ii/
+class Solution {
+    // 0ms
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> res = new ArrayList();
+        int[] dp = new int[rowIndex + 1];
+        dp[0] = 1;
+        
+        for (int i = 1; i < dp.length; i++) {
+            int prev = 1;
+            for (int j = 1; j < i; j++) {
+                int tmp = dp[j];
+                dp[j] = dp[j] + prev;
+                prev = tmp;
+            }
+            
+            dp[i] = 1;
+        }
+        
+        for (int i = 0; i < dp.length; i++) {
+            res.add(dp[i]);
+        }
+        
+        return res;
+    }
+}
 
 class Solution {
     public List<Integer> getRow(int rowIndex) {
