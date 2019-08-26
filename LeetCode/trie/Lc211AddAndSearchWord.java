fa91ac01906e15ec737748150ff280ee75b72d3f
hongyuan_weltond <ningh@umich.edu>
2019-08-26 13:40:55 -0400

    Update Lc211AddAndSearchWord.java

--- LeetCode/trie/Lc211AddAndSearchWord.java
+++ LeetCode/trie/Lc211AddAndSearchWord.java
@@ -1,5 +1,78 @@
 // https://leetcode.com/problems/add-and-search-word-data-structure-design/
 class WordDictionary {
+    
+    Trie root;
+    /** Initialize your data structure here. */
+    public WordDictionary() {
+        root = new Trie();
+    }
+    
+    /** Adds a word into the data structure. */
+    public void addWord(String word) {
+        root.add(word);
+    }
+    
+    /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+    public boolean search(String word) {
+        return search(word, 0, root);
+    }
+    
+    private boolean search(String word, int lvl, Trie t) {
+        if (lvl == word.length()) {
+            return t.isWord;
+        }
+        
+        char c = word.charAt(lvl);
+        if (c == '.') {
+            for (int i = 0; i < 26; i++) {
+                if (t.children[i] != null && search(word, lvl + 1, t.children[i])) {
+                    return true;
+                }
+            }
+            
+            return false;
+        }
+        
+        int idx = c - 'a';
+        
+        if (t.children[idx] == null) return false;
+        
+        return search(word, lvl + 1, t.children[idx]);
+    }
+}
+
+class Trie {
+    Trie[] children;
+    boolean isWord;
+    
+    public Trie() {
+        children = new Trie[26];
+        isWord = false;
+    }
+    
+    public void add(String w) {
+        Trie tmp = this;
+        for (int i = 0, len = w.length(); i < len; i++) {
+            int idx = w.charAt(i) - 'a';
+            if (tmp.children[idx] == null) {
+                tmp.children[idx] = new Trie();
+            }
+            
+            tmp = tmp.children[idx];
+        }
+        
+        tmp.isWord = true;
+    }
+}
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.addWord(word);
+ * boolean param_2 = obj.search(word);
+ */
+
+
+class WordDictionary {
     TrieNode root;
     /** Initialize your data structure here. */
     public WordDictionary() {
