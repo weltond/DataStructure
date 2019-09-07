6b68de8094a25c26a388d056acd9d0734770e14b
hongyuan_weltond <ningh@umich.edu>
2019-09-07 14:10:39 -0400

    Update Lc239SlidingWindowMaximum.java

--- LeetCode/queue/Lc239SlidingWindowMaximum.java
+++ LeetCode/queue/Lc239SlidingWindowMaximum.java
@@ -54,3 +54,35 @@ class Solution {
         }
     }
 }
+
+class Solution {
+    // 12ms 
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums == null || nums.length == 0) return new int[]{};
+        int len = nums.length;
+        int[] res = new int[len - k + 1];
+        
+        Deque<Integer> d = new LinkedList();
+        
+        for (int i = 0; i < len; i++) {
+            if (d.isEmpty() || nums[d.peekLast()] >= nums[i]) {
+                d.offerLast(i);
+            } else if (nums[d.peekLast()] < nums[i]) {
+                while (!d.isEmpty() && nums[d.peekLast()] < nums[i]) {
+                    d.pollLast();
+                }
+                d.offerLast(i);
+            }
+            
+            if (i >= k - 1) {
+                res[i - k + 1] = nums[d.peekFirst()];
+            }
+            
+            if (i - d.peekFirst() == k - 1) d.pollFirst();
+            
+            
+        }
+        
+        return res;
+    }
+}
