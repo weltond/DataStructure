159d180173ec44c385d772d758aca7a96acfd5dd
hongyuan_weltond <ningh@umich.edu>
2019-09-10 09:37:03 -0400

    Update Lc295FindMedianFromStream.java

--- LeetCode/heap/Lc295FindMedianFromStream.java
+++ LeetCode/heap/Lc295FindMedianFromStream.java
@@ -38,3 +38,38 @@ class MedianFinder {
  * obj.addNum(num);
  * double param_2 = obj.findMedian();
  */
+
+
+class MedianFinder {
+    // 137ms (10.84%)
+    PriorityQueue<Integer> lo;  // max heap
+    PriorityQueue<Integer> hi;  // min heap
+    /** initialize your data structure here. */
+    public MedianFinder() {
+        lo = new PriorityQueue<>((o1, o2) -> Integer.compare(o2, o1));
+        hi = new PriorityQueue<>();
+    }
+    
+    public void addNum(int num) {
+        // 1. add to max heap
+        lo.offer(num);
+        // 2. balance step
+        hi.offer(lo.poll());
+        
+        // 3. maintain size property that lo has one more element than hi
+        if (lo.size() < hi.size()) {
+            lo.offer(hi.poll());
+        }
+    }
+    
+    public double findMedian() {
+        return lo.size() > hi.size() ? (double) lo.peek() : (lo.peek() + hi.peek()) * 0.5;
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
