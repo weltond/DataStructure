671d5c13ca37dfbea7f67100b0f45e5bb64561d1
weltond <ningh@umich.edu>
2019-09-11 21:17:47 -0400

    add folder for companies

---
+++ LeetCode/Companies/Amazon/Lc269AlienDictionary.java
@@ -0,0 +1,58 @@
+package com.weltond.amazon;
+
+/**Problem:
+ * There is a new alien language which uses the latin alphabet. However, the order among letters are unknown to you.
+ *
+ * You receive a list of non-empty words from the dictionary, where words are sorted lexicographically
+ * by the rules of this new language.
+ *
+ * Derive the order of letters in this language.
+ *
+ * Example 1:
+ *
+ * Input:
+ * [
+ *   "wrt",
+ *   "wrf",
+ *   "er",
+ *   "ett",
+ *   "rftt"
+ * ]
+ *
+ * Output: "wertf"
+ * Example 2:
+ *
+ * Input:
+ * [
+ *   "z",
+ *   "x"
+ * ]
+ *
+ * Output: "zx"
+ * Example 3:
+ *
+ * Input:
+ * [
+ *   "z",
+ *   "x",
+ *   "z"
+ * ]
+ *
+ * Output: ""
+ *
+ * Explanation: The order is invalid, so return "".
+ * Note:
+ *
+ * You may assume all letters are in lowercase.
+ * You may assume that if a is a prefix of b, then a must appear before b in the given dictionary.
+ * If the order is invalid, return an empty string.
+ * There may be multiple valid order of letters, return any one of them is fine.
+ *
+ * https://github.com/danielmiaomtm/Algorithm-OOD-java/blob/master/Alien%20Dictionary.java
+ * 
+ * @author weltond
+ * @project LeetCode
+ * @date 3/26/2019
+ */
+public class Lc269AlienDictionary {
+}
