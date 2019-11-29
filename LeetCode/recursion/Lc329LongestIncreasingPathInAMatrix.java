43f53dff767190e87b680df4c224b27f34ec203c
hongyuan_weltond <ningh@umich.edu>
2019-11-28 23:21:52 -0500

    Update Lc329LongestIncreasingPathInAMatrix.java

--- LeetCode/recursion/Lc329LongestIncreasingPathInAMatrix.java
+++ LeetCode/recursion/Lc329LongestIncreasingPathInAMatrix.java
@@ -1,5 +1,49 @@
 // https://leetcode.com/problems/longest-increasing-path-in-a-matrix/
 
+class Solution {
+    // 8ms (72.52%)
+    public int longestIncreasingPath(int[][] matrix) {
+        if (matrix == null || matrix.length == 0) return 0;
+        
+        int[][] memo = new int[matrix.length][matrix[0].length];
+        
+        for (int[] a : memo) {
+            Arrays.fill(a, -1);
+        }
+        
+        int res = 1;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                res = Math.max(res, dfs(matrix, i, j, memo));
+            }
+        }
+        return res;
+    }
+    
+    private int dfs(int[][] arr, int i, int j, int[][] memo) {
+        if (memo[i][j] != -1) return memo[i][j];
+
+        int res = 1;
+        int[] dir = {0, -1, 0, 1, 0};
+        
+        for (int k = 0; k < 4; k++) {
+            int x = i + dir[k];
+            int y = j + dir[k + 1];
+
+            if (x >= arr.length || y >= arr[0].length || x < 0 || y < 0 || arr[x][y] >= arr[i][j]) {
+                continue;
+            }
+            
+            res = Math.max(res, dfs(arr, x, y, memo) + 1);
+        }
+        
+        memo[i][j] = res;
+        
+        return memo[i][j];
+        
+    }
+}
+
 // ====== DFS + Memoization ========
 class Solution {
     int[][] arr;
