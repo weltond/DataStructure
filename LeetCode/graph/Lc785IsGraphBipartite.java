d0ccf4ef8cc6e36867b5c4b886d5020952e0ee9e
hongyuan_weltond <ningh@umich.edu>
2019-12-14 22:51:35 -0500

    Update Lc785IsGraphBipartite.java

--- LeetCode/graph/Lc785IsGraphBipartite.java
+++ LeetCode/graph/Lc785IsGraphBipartite.java
@@ -37,3 +37,41 @@ class Solution {
         return true;
     }
 }
+
+// 6ms (11.68%)
+class Solution {
+    public boolean isBipartite(int[][] graph) {
+        Map<Integer, List<Integer>> map = new HashMap();
+        
+        for (int i = 0; i < graph.length; i++) {
+            // cannot use map.computeIfAbsent because one element might be empty
+            // e.g.: [[4],[],[4],[4],[0,2,3]]
+            List<Integer> list = new LinkedList();
+            for (int j = 0; j < graph[i].length; j++) {
+                list.add(graph[i][j]);
+            }
+            map.put(i, list);
+        }
+
+        int[] colors = new int[graph.length];
+        for (int i = 0; i < graph.length; i++) {
+            if (colors[i] == 0) {
+                if (!dfs(i, map, colors, 1)) return false;
+            }
+        }
+
+        return true;
+    }
+    
+    private boolean dfs(int start, Map<Integer, List<Integer>> map, int[] colors, int color) {
+        if (colors[start] != 0) return color == colors[start];
+        
+        colors[start] = color;
+        
+        for (int next : map.get(start)) {
+            if (!dfs(next, map, colors, -color)) return false;
+        }
+        
+        return true;
+    }
+}
