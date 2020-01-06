2e73e2892a46e75a5b88d6b40b937330adf82401
hongyuan_weltond <ningh@umich.edu>
2020-01-06 10:19:57 -0500

    Update Lc33SearchInRotatedSortedArray.java

--- LeetCode/search/binarysearch/Lc33SearchInRotatedSortedArray.java
+++ LeetCode/search/binarysearch/Lc33SearchInRotatedSortedArray.java
@@ -1,4 +1,52 @@
 // https://leetcode.com/problems/search-in-rotated-sorted-array/
+public class Solution {
+    /**
+     * @param A: an integer rotated sorted array
+     * @param target: an integer to be searched
+     * @return: an integer
+     */
+    public int search(int[] a, int target) {
+        // write your code here
+        if (a == null || a.length == 0) return -1;
+        
+        int l = 0, r = a.length - 1;
+        
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            
+            // left sorted
+            if (a[m] > a[r]) {
+                if (target <= a[m] && a[l] <= target) {
+                    return bs(a, target, l, m);
+                } else {
+                    l = m + 1;
+                }
+            } else {    // right sorted
+                if (target >= a[m] && target <= a[r]) {
+                    return bs(a, target, m, r);
+                } else {
+                    r = m - 1;
+                }
+            }
+        }
+        
+        return -1;
+    }
+    
+    private int bs(int[] a, int target, int l, int r) {
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (a[m] == target) return m;
+            else if (a[m] > target) {
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+        return -1;
+    }
+}
+
 class Solution {
     // ======= Implement 2 ===========
     // 0ms
