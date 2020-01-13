233ee153d1599940035bbdfde5ae3b816e8d885b
hongyuan_weltond <ningh@umich.edu>
2020-01-13 17:20:53 -0500

    Update Lc958CheckCompletenessOfBinaryTree.java

--- LeetCode/tree/Lc958CheckCompletenessOfBinaryTree.java
+++ LeetCode/tree/Lc958CheckCompletenessOfBinaryTree.java
@@ -4,6 +4,32 @@
 Given a binary tree, determine if it is a complete binary tree.
 */
 
+// ========== Method 2 : DFS ===========
+class Solution {
+    public boolean isCompleteTree(TreeNode root) {
+        int total = countNodes(root);
+        return helper(root, 1, total);
+    }
+    
+    private int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+    
+    private boolean helper(TreeNode root, int idx, int total) {
+        if (root == null) {
+            return true;
+        }
+        if (idx > total) {
+            return false;
+        }
+        return helper(root.left, idx * 2, total)
+            && helper(root.right, idx * 2 + 1, total);
+    }
+}
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
