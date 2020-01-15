b39a68818685b4980a918e9cd7a18037ea802bfe
hongyuan_weltond <ningh@umich.edu>
2020-01-15 12:58:52 -0500

    Update Lc240Search2DMatrixII.java

--- LeetCode/recursion/Lc240Search2DMatrixII.java
+++ LeetCode/recursion/Lc240Search2DMatrixII.java
@@ -20,7 +20,21 @@ Given target = 5, return true.
 
 Given target = 20, return false.
 */
-
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0) return false;
+        
+        int i = 0, j = matrix[0].length - 1;
+        
+        while (i < matrix.length && j >= 0) {
+            if (target == matrix[i][j]) return true;
+            else if (target < matrix[i][j]) j--;
+            else i++;
+        }
+        
+        return false;
+    }
+}
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         if (matrix == null || matrix.length < 1 || matrix[0].length < 1) return false;
