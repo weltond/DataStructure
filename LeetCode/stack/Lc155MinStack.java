9eec2c70d8bd2b791386e137ce055309e7f0faf4
hongyuan_weltond <ningh@umich.edu>
2020-01-24 08:53:15 -0500

    Update Lc155MinStack.java

--- LeetCode/stack/Lc155MinStack.java
+++ LeetCode/stack/Lc155MinStack.java
@@ -1,5 +1,96 @@
 //https://leetcode.com/problems/min-stack/
 
+// 5ms (66.23%)
+class MinStack {
+
+    Node head;
+    /** initialize your data structure here. */
+    public MinStack() {
+        head = null;
+    }
+    
+    public void push(int x) {
+        if (head == null) {
+            head = new Node(x, x, null);
+        } else {
+            head = new Node(x, Math.min(x, head.min), head);
+        }
+    }
+    
+    public void pop() {
+        head = head.next;
+    }
+    
+    public int top() {
+        return head.val;
+    }
+    
+    public int getMin() {
+        return head.min;
+    }
+}
+
+class Node {
+    int val, min;
+    Node next;
+    public Node(int val, int min, Node next) {
+        this.val = val;
+        this.min = min;
+        this.next = next;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
+// 7ms (23.48%)
+class MinStack {
+    Deque<Integer> stack;
+    int min;
+    /** initialize your data structure here. */
+    public MinStack() {
+        stack = new LinkedList();
+        min = Integer.MAX_VALUE;
+    }
+    
+    public void push(int x) {
+        if (x <= min) {
+            stack.push(min);
+            min = x;
+        }
+        stack.push(x);
+    }
+    
+    public void pop() {
+        if (stack.peek() == min) {
+            stack.pop();
+            min = stack.peek();
+        }
+        stack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return min;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
 class MinStack {
     List<Integer> arr;
     int globalMin;
