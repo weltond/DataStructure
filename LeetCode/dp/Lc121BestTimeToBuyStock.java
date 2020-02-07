3863160a8deec786df11c942df05ea9473256406
hongyuan_weltond <ningh@umich.edu>
2020-02-06 20:14:09 -0500

    Update Lc121BestTimeToBuyStock.java

--- LeetCode/dp/Lc121BestTimeToBuyStock.java
+++ LeetCode/dp/Lc121BestTimeToBuyStock.java
@@ -1,4 +1,18 @@
 // https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) return 0;
+        
+        int curMax = 0, total = 0;
+        
+        for (int i = 1; i < prices.length; i++) {
+            curMax = Math.max(0, curMax += prices[i] - prices[i - 1]);
+            total = Math.max(total, curMax);
+        }
+        
+        return total;
+    }
+}
 
 class Solution {
     public int maxProfit(int[] prices) {
