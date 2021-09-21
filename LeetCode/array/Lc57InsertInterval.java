ea693e4581771226478454fd759d779194b68428
hongyuan_weltond <ningh@umich.edu>
2021-09-20 20:58:11 -0700

    Update Lc57InsertInterval.java

--- LeetCode/array/Lc57InsertInterval.java
+++ LeetCode/array/Lc57InsertInterval.java
@@ -16,6 +16,35 @@ Output: [[1,2],[3,10],[12,16]]
 Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
 */
 
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        
+        int[][] res = new int[intervals.length + 1][2];
+        int idx = 0;    // length
+        boolean added = false;
+        for (int i = 0; i < intervals.length; i++) {
+            if (intervals[i][1] < newInterval[0]) {  // cur end < new start  => add cur
+                res[idx++] = intervals[i];
+            } else if (intervals[i][0] > newInterval[1]) {// cur start > new end   => add new, then cur
+                if (!added) {
+                    res[idx++] = newInterval;
+                    added = true;
+                }
+                
+                res[idx++] = intervals[i];
+            }else{    // cur end >= new start && cur end > new end => [1,5], [2,4]
+                newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+                newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            }
+        }
+        
+        if (!added) {
+            res[idx++] = newInterval;
+        }
+        
+        return Arrays.copyOf(res, idx);
+    }
+}
 
 class Solution {
     // ========= Approach 2==========
