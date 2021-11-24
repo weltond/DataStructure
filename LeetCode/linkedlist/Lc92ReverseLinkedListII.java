b4b5e24780a4a42f2b765a02d4454f15ba1dbd55
hongyuan_weltond <ningh@umich.edu>
2021-11-24 12:22:39 -0800

    Update Lc92ReverseLinkedListII.java

--- LeetCode/linkedlist/Lc92ReverseLinkedListII.java
+++ LeetCode/linkedlist/Lc92ReverseLinkedListII.java
@@ -13,8 +13,56 @@ Output: 1->4->3->2->5->NULL
  *     ListNode(int x) { val = x; }
  * }
  */
+
+// 0ms
 class Solution {
-    // Like No.25 Reverse Nodes in K Group
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;  // use dummy because head might change
+
+        int step = right - left + 1;
+
+        // find the "left" node
+        while (head != null && left > 1) {
+            prev.next = head;
+            prev = head;
+
+            head = head.next;
+            left--;
+        }
+
+        ListNode newHead = reverse(head, step);
+
+        // connect left's prev to new head
+        prev.next = newHead;
+        
+        return dummy.next;
+    }
+
+    // reverse the window
+    // return new head
+    private ListNode reverse(ListNode cur, int step) {
+        ListNode newEnd = cur;
+
+        ListNode prev = null;
+        while (cur != null && step > 0) {
+            ListNode next = cur.next;
+            cur.next = prev;
+            prev = cur;
+
+            cur = next;
+            step--;
+        }
+        // cur is right's next
+        newEnd.next = cur; // connect newEnd and right's next
+
+        return prev;    // prev is new head
+    }
+}
+
+// Like No.25 Reverse Nodes in K Group
+class Solution {
     public ListNode reverseBetween(ListNode head, int m, int n) {
         if (head == null || head.next == null || m == n) return head;
         
