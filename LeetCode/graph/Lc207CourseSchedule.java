a5d12993256c4e5db318945a57116a5b719864a4
hongyuan_weltond <ningh@umich.edu>
2021-12-20 11:30:57 -0800

    Update Lc207CourseSchedule.java

--- LeetCode/graph/Lc207CourseSchedule.java
+++ LeetCode/graph/Lc207CourseSchedule.java
@@ -1,5 +1,10 @@
 // https://leetcode.com/problems/course-schedule/
 
+/**
+There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. 
+You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that 
+you must take course bi first if you want to take course ai.
+*/
 class Solution {
     private List<List<Integer>> buildGraph(int[][] arr, int numCourses) {
         List<List<Integer>> graph = new ArrayList();
