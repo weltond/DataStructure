e3f8ad7e08df74d9d3d5eb7d71ca7e5bd1b10ffb
hongyuan_weltond <ningh@umich.edu>
2021-12-20 21:27:27 -0800

    Update Lc153FindMinInRotatedArray.java

--- LeetCode/array/Lc153FindMinInRotatedArray.java
+++ LeetCode/array/Lc153FindMinInRotatedArray.java
@@ -2,6 +2,23 @@
 
 class Solution {
     public int findMin(int[] nums) {
+        int l = 0, r = nums.length - 1;
+
+        while (l < r - 1) {
+            int m = l + (r - l) / 2;
+
+            // r is the target
+            if (nums[m] > nums[r]) l = m;
+            else r = m;
+        }
+        
+        // l + 1 == r
+        return Math.min(nums[l], nums[r]);
+    }
+}
+
+class Solution {
+    public int findMin(int[] nums) {
         if (nums == null) return -1;
         
         int left = 0, right = nums.length - 1;
