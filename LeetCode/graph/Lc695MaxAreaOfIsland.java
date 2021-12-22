fc4a9b7ad93c6b5600ac1ddf37f3c23b50fe9539
hongyuan_weltond <ningh@umich.edu>
2021-12-22 14:22:30 -0800

    Update Lc695MaxAreaOfIsland.java

--- LeetCode/graph/Lc695MaxAreaOfIsland.java
+++ LeetCode/graph/Lc695MaxAreaOfIsland.java
@@ -1,3 +1,38 @@
+// 2ms (99.31%)
+class Solution {
+    int[] dir = {0, 1, 0, -1, 0};
+    public int maxAreaOfIsland(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+
+        int res = 0;
+        for (int i = 0; i < m ; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    res = Math.max(res, dfs(grid, i, j));
+                }
+            }
+        }
+
+        return res;
+    }
+
+    private int dfs(int[][] grid, int x, int y) {
+        if (x >= grid.length || y >= grid[0].length || x < 0 || y < 0 ||
+        grid[x][y] == 0) {
+            return 0;
+        }
+
+        int count = 1;
+        grid[x][y] = 0;
+
+        for (int i = 0; i < 4; i++) {
+            count += dfs(grid, x + dir[i], y + dir[i + 1]);
+        }
+
+        return count;
+    }
+}
+
 class Solution {
     public int maxAreaOfIsland(int[][] grid) {
         if (grid == null || grid.length == 0) return 0;
