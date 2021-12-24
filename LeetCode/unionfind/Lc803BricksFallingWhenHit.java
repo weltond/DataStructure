af768dd7fbf69061efb986d697e84d16a08ce032
hongyuan_weltond <ningh@umich.edu>
2021-12-24 07:57:37 -0800

    Update Lc803BricksFallingWhenHit.java

--- LeetCode/unionfind/Lc803BricksFallingWhenHit.java
+++ LeetCode/unionfind/Lc803BricksFallingWhenHit.java
@@ -1,4 +1,146 @@
 // https://leetcode.com/problems/bricks-falling-when-hit/
+
+// 23ms (51.27%)
+class Solution {
+    int[] dir = {0, 1, 0, -1, 0};
+
+    public int[] hitBricks(int[][] grid, int[][] hits) {
+        int m = grid.length, n = grid[0].length;
+        UF uf = new UF(m * n + 1);
+        int dummy = m * n;
+
+        // 1. we hit grid first to get final status
+        for (int[] hit : hits) {
+            int x = hit[0], y = hit[1];
+            if (grid[x][y] == 1) {
+                grid[x][y] = 2;
+            }
+        }
+
+        // 2. union grid. top row is connected to the dummy
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    internalUnion(grid, i, j, uf, dummy);
+                }
+            }
+        }
+
+        // for (int i = 0; i < m * n + 1; i++) {
+        //     System.out.print(uf.find(i) + " ");
+        // }
+
+        //System.out.println();
+        // 3. count remaining bricks for final status
+        int countFinal = uf.weight[uf.find(dummy)];
+
+        // 4. reverse traverse hits to recover the grid.
+        int[] res = new int[hits.length];
+        for (int i = hits.length - 1; i >= 0; i--) {
+            int x = hits[i][0], y = hits[i][1];
+
+            // find how many bricks were connect to top through this brick before hitting
+            if (grid[x][y] == 2) {
+                internalUnion(grid, x, y, uf, dummy);
+                grid[x][y] = 1; // reset 
+            }
+
+            // for (int o = 0; o < m * n + 1; o++) {
+            //     System.out.print(uf.find(o) + " ");
+            // }
+
+            int countBeforeHittingCurrent = uf.weight[uf.find(dummy)];
+            res[i] = Math.max(0, countBeforeHittingCurrent - countFinal - 1);
+
+            countFinal = countBeforeHittingCurrent;
+        }
+
+        return res;
+    }
+
+    private void internalUnion(int[][] grid, int x, int y, UF uf, int dummy) {
+        int m = grid.length, n = grid[0].length;
+        for (int k = 0; k < 4; k++) {
+            int nx = x + dir[k], ny = y + dir[k + 1];
+            if (nx < 0 || ny < 0 || nx >= m || ny >= n || grid[nx][ny] != 1) continue;
+
+            uf.union(x * n + y, nx * n + ny);
+        }
+
+        // connect with dummy for first row
+        // IMPORTANT: must do this for step 4, in case top row brick is removed
+        if (x == 0) {
+            uf.union(dummy, y);
+        }
+    }
+}
+
+class UF {
+    // 记录连通分量
+    private int count;
+    
+    // 节点X的节点是parent[x]
+    public int[] parent;
+    
+    // 记录树的权重
+    public int[] weight;
+    
+    public UF(int n) {
+        // 一开始互不连通
+        this.count = n;
+        weight = new int[n];
+        
+        // 父节点指针初始指向自己
+        parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            weight[i] = 1;
+        }
+    }
+
+    /* 判断 p 和 q 是否互相连通 */
+    public boolean connected(int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+        // 处于同一棵树上的节点，相互连通
+        return rootP == rootQ;
+    }
+
+    public void union(int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+        
+        if (rootP == rootQ) return;
+        
+        // union two trees
+        // smaller is connected under larger
+        if (weight[rootP] >= weight[rootQ]) {
+            parent[rootQ] = rootP;
+            weight[rootP] += weight[rootQ];
+        } else {
+            parent[rootP] = rootQ;
+            weight[rootQ] += weight[rootP];
+        }
+        
+        count--;    //两个分量合二为一
+    }
+    
+    // find x's root node
+    public int find(int x) {
+        while (parent[x] != x) {
+            // 进行路径压缩
+            parent[x] = parent[parent[x]];
+            x = parent[x];
+        }
+        
+        return x;
+    }
+    
+    public int count() {
+        return this.count;
+    }
+}
+
 class Solution {
     public int[] hitBricks(int[][] grid, int[][] hits) {
         int m = grid.length;
