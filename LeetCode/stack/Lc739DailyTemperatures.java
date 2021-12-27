541287b105c9a5f3e9427bb4fcbbc88849ec21ce
hongyuan_weltond <ningh@umich.edu>
2021-12-27 10:06:05 -0800

    Update Lc739DailyTemperatures.java

--- LeetCode/stack/Lc739DailyTemperatures.java
+++ LeetCode/stack/Lc739DailyTemperatures.java
@@ -1,6 +1,32 @@
 // https://leetcode.com/problems/daily-temperatures/
 
 class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Deque<Integer> stack = new LinkedList<>();
+        int[] res = new int[temperatures.length];
+
+        for (int i = temperatures.length - 1; i >= 0; i--) {
+            int val = temperatures[i];
+
+            // if val is greater than stack.peek()
+            // keep popping until stack.peek() > val, this is the date that have higher temp than i.
+            while (!stack.isEmpty() && temperatures[stack.peek()] <= val) {
+                stack.pop();
+            }
+
+            // stack is empty means no future days have higher temp
+            // otherwise, stack.peek() is the first date that has higher temp than current i.
+            res[i] = stack.isEmpty() ? 0 : stack.peek() - i;
+
+            // push to stack because current i might be the first future date with higher tmp
+            stack.push(i);
+        }
+
+        return res;
+    }
+}
+
+class Solution {
     public int[] dailyTemperatures(int[] T) {
         int[] res = new int[T.length];
         Deque<Integer> stack = new ArrayDeque();    // Stack stores index, NOT value
