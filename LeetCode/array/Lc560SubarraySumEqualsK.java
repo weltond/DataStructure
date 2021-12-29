46252b8bb5993481ab7bfc07ac38df5f8944e13b
hongyuan_weltond <ningh@umich.edu>
2021-12-28 17:07:35 -0800

    Update Lc560SubarraySumEqualsK.java

--- LeetCode/array/Lc560SubarraySumEqualsK.java
+++ LeetCode/array/Lc560SubarraySumEqualsK.java
@@ -1,4 +1,32 @@
 // https://leetcode.com/problems/subarray-sum-equals-k/
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        // <presum, freq>
+        Map<Integer, Integer> presum = new HashMap<>();
+        int res = 0, sum_i = 0;
+        
+        // base case
+        presum.put(0, 1);
+
+        for (int i = 0; i < nums.length; i++) {
+            sum_i += nums[i];
+
+            // presum of nums[0..j]
+            // presum[j] = presum[i] - k
+            int sum_j = sum_i - k;
+
+            // update if sum_j already exists
+            if (presum.containsKey(sum_j)) {
+                res += presum.get(sum_j);
+            }
+
+            // freq for presum of nums[0..i]
+            presum.put(sum_i, presum.getOrDefault(sum_i, 0) + 1);
+        }
+
+        return res;
+    }
+}
 
 class Solution {
     public int subarraySum(int[] nums, int k) {
