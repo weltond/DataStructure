8fe3d19a8b38fd8446a5c3e12ca004e733ff67ec
hongyuan_weltond <ningh@umich.edu>
2022-01-17 10:00:42 -0800

    Update Lc160IntersectionOfTwoLinkedList.java

--- LeetCode/linkedlist/Lc160IntersectionOfTwoLinkedList.java
+++ LeetCode/linkedlist/Lc160IntersectionOfTwoLinkedList.java
@@ -11,6 +11,21 @@
  *     }
  * }
  */
+
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode h1 = headA, h2 = headB;
+
+        while (h1 != null || h2 != null) {
+            if (h1 == h2) return h1;
+            h1 = h1 == null ? headB : h1.next;
+            h2 = h2 == null ? headA : h2.next;
+        }
+
+        return null;
+    }
+}
+
 public class Solution {
     
     // ===== Method 2: Two Pointers ======
