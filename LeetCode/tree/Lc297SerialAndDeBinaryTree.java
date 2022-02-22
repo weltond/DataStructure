a150f933aa92d708287b579a812a42eb63f2bf59
hongyuan_weltond <ningh@umich.edu>
2022-02-22 13:20:42 -0800

    Update Lc297SerialAndDeBinaryTree.java

--- LeetCode/tree/Lc297SerialAndDeBinaryTree.java
+++ LeetCode/tree/Lc297SerialAndDeBinaryTree.java
@@ -1,5 +1,65 @@
 // https://leetcode.com/problems/serialize-and-deserialize-binary-tree/
 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Codec {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        String s = dfs(root);   // [1,2,3,null,null,4,5] ==> 1',2,3',4,5
+
+        return s;
+    }
+    
+    private String dfs(TreeNode root) {
+        if (root == null) return "/";
+        
+        if (root.left == null && root.right == null) return root.val + "";
+        
+        
+        return root.val + "'," + dfs(root.left) + "," + dfs(root.right);
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        String[] strs = data.split(",");
+        
+        return helper(strs);
+    }
+    
+    int idx = 0;
+    private TreeNode helper(String[] data) {
+        String val = data[idx++];
+        if (val.equals("/")) return null;
+        
+        // leaf
+        if (!val.endsWith("'")) {
+            return new TreeNode(Integer.parseInt(val));
+        }
+        
+        String rootStr = val.split("'")[0];
+        TreeNode root = new TreeNode(Integer.parseInt(rootStr));
+        
+        root.left = helper(data);
+        root.right = helper(data);
+        
+        return root;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser = new Codec();
+// Codec deser = new Codec();
+// TreeNode ans = deser.deserialize(ser.serialize(root));
+
+
 
 /**
  * Definition for a binary tree node.
