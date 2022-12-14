dfaf54ead17e007b2ecb238ba04e6e27bd20e34c
hongyuan_weltond <ningh@umich.edu>
2022-12-13 20:40:57 -0800

    Update Lc124BinaryTreeMaxPathSum.java

--- LeetCode/tree/Lc124BinaryTreeMaxPathSum.java
+++ LeetCode/tree/Lc124BinaryTreeMaxPathSum.java
@@ -31,6 +31,41 @@ Output: 42
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int res = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        dfs(root);
+
+        return res;
+    }
+
+    private int dfs(TreeNode root) {
+        if (root == null) return 0;
+
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+
+        res = Math.max(res, root.val + left + right);
+
+        return Math.max(0, root.val + Math.max(left, right));
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
  *     TreeNode(int x) { val = x; }
  * }
  */
