d1acc7b9a0963126dc16dffecfe93acf613e8b59
hongyuan_weltond <ningh@umich.edu>
2022-12-27 14:37:59 -0800

    Update Lc42TrappingRainWater.java

--- LeetCode/array/Lc42TrappingRainWater.java
+++ LeetCode/array/Lc42TrappingRainWater.java
@@ -10,7 +10,7 @@ Output: 6
 */
 
 class Solution {
-    // ======== Method 2: DP ==========
+    // ======== Method 3: DP ==========
     // 1ms (99.37%) O(n) O(n)
     public int trap(int[] height) {
        if (height == null || height.length == 0) return 0;
@@ -33,6 +33,75 @@ class Solution {
         
         return ans;
     }
+    
+    // 1ms (99.56%)
+    class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+        int[] left = new int[n];
+        
+        int[] right = new int[n];
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                left[i] = height[i];
+                continue;
+            }
+            left[i] = Math.max(left[i - 1], height[i - 1]);
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (i == n - 1) {
+                right[i] = height[i];
+                continue;
+            }
+            right[i] = Math.max(right[i + 1], height[i + 1]);
+        }
+
+        int res = 0;
+
+        for (int i = 0; i < n; i++) {
+            res += Math.max(0, Math.min(left[i], right[i]) - height[i]);
+        }
+
+        return res;
+    }
+
+}
+    
+    // ======== Method 2 : Stack ========
+    // 2ms (60.39%)
+    class Solution {
+    // Increase Monolonic Stack
+    public int trap(int[] height) {
+        Deque<Integer> stack = new LinkedList<>();  // index
+
+        int idx = 0, res = 0;
+
+        // stack stores info of left highest hight of current bar
+        while (idx < height.length) {
+            int val = height[idx];
+            // if cur is greater than top, continue popping until 
+            //  cur is less than top or stack is empty
+            //  we calculate res exclude current bar
+            while (!stack.isEmpty() && height[idx] > height[stack.peek()]) {
+                int h = height[stack.pop()];    
+
+                // no left bound
+                if(stack.isEmpty()) {
+                    break;
+                }
+
+                int width = idx - stack.peek() - 1;
+                int minBound = Math.min(height[stack.peek()], height[idx]);
+                res += width * (minBound- h);
+            }
+
+            stack.push(idx++);
+        }
+
+        return res;
+    }
+}
     
     // ======== Method 1: Brute Force ==========
     // 59ms(5.53%) O(n2) O(1)
